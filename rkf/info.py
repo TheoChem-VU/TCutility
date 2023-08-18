76797def36efadf3e3f8f756b2e70fcf8c2f52fa
Yuman Hordijk <yumanhordijk@gmail.com>
2023-08-19 01:47:38 +0200

    Improved docstrings and simplified parts of the status logic

--- rkf/info.py
+++ rkf/info.py
@@ -1,6 +1,4 @@
-"""This module provides info about calculations done using AMS based on rkf files. 
-This includes information about the engine used (ADF, DFTB, BAND, ...), general calculation settings such as relativistic corrections, symmetries, status of the calculations, ...
-This information are then used in further analysis programs.
+"""This module provides basic and general information about calculations done using AMS given a calculation directory. This includes information about the engine used (ADF, DFTB, BAND, ...), general information such as timings, files, status of the calculation, ... This information is used in further analysis programs.
 """
 
 import numpy as np
@@ -23,16 +21,22 @@ def get_calc_files(calc_dir: str) -> dict:
     # collect all files in the current directory and subdirectories
     files = []
     for root, _, files_ in os.walk(calc_dir):
+        # some results are stored in dirs called {name}.results, if the calculations uses fragments there will be additional dirs called
+        # {name}.{fragname}.results, which do not contain new information as the required info is copied over to {name}.results. Therefore
+        # we should skip the fragment directories 
         if os.path.split(root)[1].endswith('.results') and len(os.path.split(root)[1].split('.')) > 2:
             continue
+
+        # if a calculation is running or did not finish correctly there might be tmp. dirs, which should be ignored
         if os.path.split(root)[1].startswith('tmp.'):
             continue
+
+        # the rest can all be stored
         files.extend([j(root, file) for file in files_])
 
     # parse the filenames
-    # first parse the rkf files. This will tell us how many fragments there are in the calculation. There should be only two rkf files for a normal calculation
-    # there are therefore n_file_ending_in_rkf - 2 fragments
     ret = {}
+    ret['root'] = os.path.abspath(calc_dir)
     for file in files:
         # rkf files are either called /{program}.rkf, or /{name}.{program}.rkf, so we must parse them in a special way
         if file.endswith('.rkf'):
@@ -41,6 +45,7 @@ def get_calc_files(calc_dir: str) -> dict:
             f = f'{parts[-2]}.rkf'
             ret[f] = os.path.abspath(file)
 
+        # logfiles are generally called either {name}.log or {name}.logfile
         if file.endswith('.logfile') or file.endswith('.log') or file.endswith('.err'):
             f = os.path.split(file)[1]
             parts = f.split('.')
@@ -62,12 +67,12 @@ def get_calc_info(calc_dir: str) -> dictfunc.DotDict:
     '''
     ret = dictfunc.DotDict()
     ret.files = get_calc_files(calc_dir)
-    ret.files.root = os.path.abspath(calc_dir)
 
     reader_ams = plams.KFReader(ret.files['ams.rkf'])
 
     # store information about the version of AMS
     ret.ams_version.string = reader_ams.read('General', 'release')
+    # decompose the version string
     ret.ams_version.major = ret.ams_version.string.split('.')[0]
     ret.ams_version.minor = ret.ams_version.string.split()[0].split('.')[1]
     ret.ams_version.micro = ret.ams_version.string.split()[1]
@@ -107,10 +112,15 @@ def calculation_status(calc_dir: str) -> dictfunc.DotDict:
         reader: plams.KFReader or plams.KFFile object pointing to the desired calculation
 
     Returns:
-        Tuple containing (success, reasons), where success indicates whether the calculation has successfully finished.
-        If it is not finished, reasons will contain the reasons that the calculation is not finished.
+        Dictionary containing information about the calculation status
+        success: bool - True if calculation reported normal termination, with or without warnings, False otherwise
+        reasons: list[str] - list of reasons to explain the status, they can be errors, warnings, etc.
+        name: str - calculation status written as a string, ex. "RUNNING", "FAILED", etc.
+        code: str - calculation status written as a single character, ex. "S", "F", etc.
+
     '''
     files = get_calc_files(calc_dir)
+    # termination info is stored in ams.rkf
     reader_ams = plams.KFReader(files['ams.rkf'])
 
     ret = dictfunc.DotDict()
@@ -120,6 +130,16 @@ def calculation_status(calc_dir: str) -> dictfunc.DotDict:
     ret.reasons = []
 
     termination_status = reader_ams.read('General', 'termination status').strip()
+
+    # parse the logfile to find errors and warnings
+    if 'log' in files:
+        with open(files['log']) as logfile:
+            for line in logfile.readlines():
+                # the first 25 characters include the timestamp and two spaces
+                line_ = line.strip()[25:]
+                # errors and warnings have a predictable format
+                if line_.startswith('ERROR:') or line_.startswith('WARNING: '):
+                    ret.reasons.append(line_)
 
     if termination_status == 'NORMAL TERMINATION':
         ret.success = True
@@ -139,22 +159,19 @@ def calculation_status(calc_dir: str) -> dictfunc.DotDict:
         ret.code = 'U'
         return ret
 
-    if termination_status in ('NORMAL TERMINATION with warnings', 'NORMAL TERMINATION with errors'):
-        if termination_status == 'NORMAL TERMINATION with warnings':
-            ret.success = True
-            ret.name = 'SUCCESS(W)'
-            ret.code = 'W'
-        else:
-            ret.name = 'FAILED'
-            ret.code = 'F'
-
-        with open(files['log']) as logfile:
-            for line in logfile.readlines():
-                line_ = line.strip()[25:]
-                if line_.startswith('ERROR:') or line_.startswith('WARNING: '):
-                    ret.reasons.append(line_)
+    if termination_status == 'NORMAL TERMINATION with warnings':
+        ret.success = True
+        ret.name = 'SUCCESS(W)'
+        ret.code = 'W'
         return ret
 
+    if termination_status == 'NORMAL TERMINATION with warnings':
+        ret.name = 'FAILED'
+        ret.code = 'F'
+        return ret
+
+    # if we have not excited the function yet we do not know what the status is
+    # probably means that there was a parsing error in ams, which will be placed in termination status
     ret.reasons.append(termination_status)
     ret.success = False
     ret.name = 'UNKNOWN'
