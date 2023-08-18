0a5ef5dba69a8c1f532fcc856436c2ae08471d14
Yuman Hordijk <yumanhordijk@gmail.com>
2023-08-19 01:47:50 +0200

    now also testing adf

--- test/rkf/test.py
+++ test/rkf/test.py
@@ -1,6 +1,8 @@
 from yutility import log
 import os
 from TCutility.rkf import info as rkf_info
+from TCutility.rkf import adf as rkf_adf
+
 
 for calc_dir in os.listdir():
     if calc_dir == '.DS_Store':
@@ -8,5 +10,15 @@ for calc_dir in os.listdir():
     if not os.path.isdir(calc_dir):
         continue
 
-    log.log(rkf_info.get_calc_info(calc_dir))
+    info = rkf_info.get_calc_info(calc_dir)
+    log.log(info)
+    try:
+        log.log(rkf_adf.get_calc_settings(info))
+    except:
+        pass
+    # if info.status.success:
+    #     log.log('The job succeeded')
+    # else:
+    #     log.log(f'The job did not succeed')
+    # log.log(info)
     log.log()
