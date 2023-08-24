cc6eab41223c2b418405d12551e48e83789a6812
Yuman Hordijk <yumanhordijk@gmail.com>
2023-08-24 22:32:39 +0200

    Added support for dftb

--- rkf/__init__.py
+++ rkf/__init__.py
@@ -1,5 +1,5 @@
 from yutility import dictfunc
-from . import adf, info
+from . import adf, dftb, info
 
 
 def read(calc_dir: str) -> dictfunc.DotDict:
@@ -7,4 +7,6 @@ def read(calc_dir: str) -> dictfunc.DotDict:
     ret.update(info.get_calc_info(calc_dir))
     if ret.engine == 'adf':
         ret.adf = adf.get_calc_settings(ret)
+    elif ret.engine == 'dftb':
+        ret.dftb = dftb.get_calc_settings(ret)
     return ret
