133e01a9ecb6c246b4f6f7d862fbecca682a2908
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-08-27 23:07:37 +0200

    Added documentation building

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+
+current_dir = os.path.dirname(__file__)
+target_dir = os.path.abspath(os.path.join(current_dir, "../"))
+sys.path.insert(0, target_dir)
+
+print('TARGET DIR', target_dir)
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TCutility'
+copyright = '2023, Yuman Hordijk, Siebe Lekanne Deprez'
+author = 'Yuman Hordijk, Siebe Lekanne Deprez'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    "sphinx.ext.autodoc"
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'  # pip install pydata-sphinx-theme
+html_static_path = ['_static']
+
+
+# custom variables
+rst_epilog = f"""
+.. |ProjectVersion| replace:: v{release}
+"""
---
+++ docs/rkf.rst
@@ -0,0 +1,45 @@
+rkf package
+===========
+
+Submodules
+----------
+
+rkf.adf module
+--------------
+
+.. automodule:: rkf.adf
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rkf.cache module
+----------------
+
+.. automodule:: rkf.cache
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rkf.dftb module
+---------------
+
+.. automodule:: rkf.dftb
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+rkf.info module
+---------------
+
+.. automodule:: rkf.info
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: rkf
+   :members:
+   :undoc-members:
+   :show-inheritance:
