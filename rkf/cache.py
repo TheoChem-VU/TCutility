bdc0bd2d648dc19856268717988bc6fd3ebb3520
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-08-27 23:07:12 +0200

    Added docstrings and typehints

--- rkf/cache.py
+++ rkf/cache.py
@@ -3,33 +3,50 @@ Tiny module that handles caching rkf files.
 rkf files take a long time to open (especially {engine}.rkf), so it is better to open them once and cache them for later use
 '''
 from scm import plams
+from typing import Union, Any
 
 # the actual cache is stored in this dict
 _cache = {}
 
 
 class TrackKFReader(plams.KFReader):
-    # subclass of plams.KFReader that also tracks the variables that were read
-    # this can be useful later on to gather which variables are important
+    """Subclass of plams.KFReader that also tracks the variables that were read. This class can be useful to figure out which variables are important. For example, we can then trim rkf files to reduce their filesizes."""
     def __init__(self, *args, **kwargs):
         self.tracker = []
         super().__init__(*args, **kwargs)
 
-    def read(self, section, variable):
+    def read(self, section: str, variable: str) -> Any:
+        '''Read a variable from a section of an rkf file and store the accessed section and variable in the tracker.
+
+        Args:
+            section: Name of the section to read from.
+            variable: Name of the variable inside the section.
+
+        Returns:
+            Any value returned will be automatically converted into the correct type. E.g. text-based data will be converted to a string, numbers to floats or integers, etc.'''
+            
         self.tracker.append((section, variable))
         return super().read(section, variable)
 
 
-def store(reader):
-    # store an rkf reader in the cache
-    # it can later be indexed by its path
+def store(reader: plams.KFReader) -> None:
+    '''Store an rkf reader in the cache. It can later be indexed by its path.
+
+    Args:
+        reader: An rkf file reader object.'''
     path = reader.path
     _cache[path] = reader
 
 
-def get(path):
-    # get an rkf reader from the cache
+def get(path: str) -> plams.KFReader:
+    '''Retrieve an rkf reader from storage using its path. If the file was not opened yet, open it first and then store and return the new object.
 
+    Args:
+        path: path to the rkf file location.
+
+    Returns:
+        An rkf file reader that can be used for reading data from a calculation.
+    '''
     # if the path is already in the cache, simply return it
     if path in _cache:
         return _cache[path]
@@ -39,11 +56,11 @@ def get(path):
     return reader
 
 
-def unload(arg):
-    # arg can either be a path or reader object
-    # since rkf files can be quite large, we should not forget to unload
-    # them after we have finished reading from them
+def unload(arg: Union[str, plams.KFReader]):
+    '''Delete an rkf reader from storage. Since rkf files can be quite large, we should not forget to unload them after we have finished reading from them, lest we run into memory issues.
 
+    Args:
+        arg: Either a path pointing to the rkf file location or the reader itself.'''
     # if the arg is a key in the cache we will delete the key
     if arg in _cache:
         del _cache[arg]
