f26551485968df0ed92fe51dca765567c06734b1
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-08-28 18:28:56 +0200

    Added missing path

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,10 @@ import sys
 
 
 current_dir = os.path.dirname(__file__)
+
+target_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.insert(0, target_dir)
+
 target_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
 sys.path.insert(0, target_dir)
 
