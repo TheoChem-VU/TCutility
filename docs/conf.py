bc4e8a8651e5eaca2f776e52d755d0b4dda1afe5
Yuman Hordijk <yumanhordijk@gmail.com>
2023-08-29 14:43:00 +0200

    Removed unnecessary imports

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,6 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
-
 
 
 # -- Project information -----------------------------------------------------
