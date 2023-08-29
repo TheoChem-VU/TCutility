4120ef380951b9bc7e110cee2c6f37519e4034df
Yuman Hordijk <yumanhordijk@gmail.com>
2023-08-29 14:54:45 +0200

    Added a new test-case

--- test/test_results.py
+++ test/test_results.py
@@ -7,3 +7,7 @@ j = os.path.join
 def test_read_dft() -> None:
     res = results.read(j('test', 'fixtures', 'DFT_EDA'))
     assert isinstance(res, results.Result)
+
+def test_dft_engine() -> None:
+    res = results.read(j('test', 'fixtures', 'DFT_EDA'))
+    assert res.engine == 'adf'
