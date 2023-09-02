5f00e7459966a3a5e5bde18a11002b57d8e8cdd1
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-09-02 12:03:25 +0200

    Added properties for ADF calculations, currently including energies and vibrations

--- TCutility/results/adf.py
+++ TCutility/results/adf.py
@@ -1,6 +1,9 @@
 from TCutility.results import cache, Result
 
 
+ha2kcalmol = 627.509474
+
+
 def get_calc_settings(info: Result) -> Result:
     '''
     Function to read useful calculation settings from kf reader
@@ -56,3 +59,32 @@ def get_properties(info: Result) -> Result:
     '''
     
     '''
+
+    assert info.engine == 'adf', f'This function reads ADF data, not {info.engine} data'
+    assert 'adf.rkf' in info.files, f'Missing adf.rkf file, [{", ".join([": ".join(item) for item in info.files.items()])}]'
+
+    reader_adf = cache.get(info.files['adf.rkf'])
+    reader_ams = cache.get(info.files['ams.rkf'])
+    ret = Result()
+
+    # read energies (given in Ha in rkf files)
+    ret.energy.bond = reader_adf.read('Energy', 'Bond Energy') * ha2kcalmol
+    ret.energy.elstat = reader_adf.read('Energy', 'elstat') * ha2kcalmol
+    ret.energy.orbint.total = reader_adf.read('Energy', 'Orb.Int. Total') * ha2kcalmol
+    for symlabel in info.adf.symmetry.labels:
+        ret.energy.orbint[symlabel] = reader_adf.read('Energy', f'Orb.Int. {symlabel}') * ha2kcalmol
+    ret.energy.pauli.total = reader_adf.read('Energy', 'Pauli Total') * ha2kcalmol
+    ret.energy.dispersion = reader_adf.read('Energy', 'Dispersion Energy') * ha2kcalmol
+
+    # vibrational information
+    ret.vibrations.number_of_modes = reader_adf.read('Vibrations', 'nNormalModes')
+    freqs = reader_adf.read('Vibrations', 'Frequencies[cm-1]')
+    ints = reader_adf.read('Vibrations', 'Intensities[km/mol]')
+    ret.vibrations.frequencies = freqs if isinstance(freqs, list) else [freqs]
+    ret.vibrations.intensities = ints if isinstance(ints, list) else [ints]
+    ret.vibrations.number_of_imag_modes = len([freq for freq in freqs if freq < 0])
+    ret.vibrations.modes = []    
+    for i in range(ret.vibrations.number_of_modes):
+        ret.vibrations.modes.append(reader_adf.read('Vibrations', f'NoWeightNormalMode({i+1})'))
+
+    return ret
