53ae057bc07af96d5a3ade631aa8053ebc0f4c70
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-09-02 22:25:22 +0200

    Moved example usage to shared folder as rst

--- TCutility/results/__init__.py
+++ TCutility/results/__init__.py
@@ -4,22 +4,8 @@ This includes information about the engine used (ADF, DFTB, BAND, ...), general 
 This information is used in further analysis programs.
 
 Typical usage example:
-    
-    >>> import TCutility.results
-    >>> calc_dir = '../test/fixtures/ethanol'
-    >>> info = TCutility.results.read(calc_dir)
-    >>> info.engine
-    adf
-    >>> info.ams_version.full
-    2022.103 r104886 (2022-06-17)
-    >>> info.adf.symmetry
-    {'group': 'C(S)', 'labels': ['AA', 'AAA']}
-    >>> info.properties.energy.orbint
-    {'AA': -2738.644830445246,
-     'AAA': -1056.9706925183411,
-     'total': -3795.615522963587}
-    >>> info.properties.vibrations.number_of_imag_modes
-    0
+
+.. literalinclude:: ../docs/shared/usage_results.rst
 """
 
 from . import result
