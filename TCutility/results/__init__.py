e3c3c241a14c1d45aeda1e115b4d2ea10750f7fa
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-09-02 12:03:11 +0200

    Added engine-specific properties getting for calculations

--- TCutility/results/__init__.py
+++ TCutility/results/__init__.py
@@ -8,8 +8,18 @@ Typical usage example:
     >>> import TCutility.results
     >>> calc_dir = '../test/fixtures/ethanol'
     >>> info = TCutility.results.read(calc_dir)
-    >>> print(info.engine)
-    >>> print(info.properties.energy.bond)
+    >>> info.engine
+    adf
+    >>> info.ams_version.full
+    2022.103 r104886 (2022-06-17)
+    >>> info.adf.symmetry
+    {'group': 'C(S)', 'labels': ['AA', 'AAA']}
+    >>> info.properties.energy.orbint
+    {'AA': -2738.644830445246,
+     'AAA': -1056.9706925183411,
+     'total': -3795.615522963587}
+    >>> info.properties.vibrations.number_of_imag_modes
+    0
 """
 
 from . import result
@@ -31,6 +41,8 @@ def read(calc_dir: str) -> Result:
     ret.update(ams.get_ams_info(calc_dir))
     if ret.engine == 'adf':
         ret.adf = adf.get_calc_settings(ret)
+        ret.properties = adf.get_properties(ret)
     elif ret.engine == 'dftb':
         ret.dftb = dftb.get_calc_settings(ret)
+        ret.properties = dftb.get_properties(ret)
     return ret
