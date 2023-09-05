d38e3ea3eafb10184a09f3afa13f39d889405bdb
Yuman Hordijk <yumanhordijk@gmail.com>
2023-09-05 17:04:04 +0200

    Ignoring some Ruff checks

--- TCutility/__init__.py
+++ TCutility/__init__.py
@@ -1,3 +1,3 @@
-ensure_list = lambda x: [x] if not isinstance(x, (list, tuple, set)) else x
+ensure_list = lambda x: [x] if not isinstance(x, (list, tuple, set)) else x  # noqa: E731
 
-from TCutility import results, constants  # noqa: F401
+from TCutility import results, constants  # noqa: F401, E402
