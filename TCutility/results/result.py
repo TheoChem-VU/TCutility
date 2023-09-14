04f40de0556d15693f626947d2151261c039f6c0
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-09-14 22:48:16 +0200

    getattr and setattr now simply point to getitem and setitem

--- TCutility/results/result.py
+++ TCutility/results/result.py
@@ -3,19 +3,22 @@
 
 class Result(dict):
     '''Class used for storing results from AMS calculations. The class is functionally a dictionary, but allows dot notation to access variables in the dictionary.'''
-    def __getattr__(self, key):
-        if key not in self:
-            val = Result()
-            self.__setitem__(key, val)
-            return val
-
-        val = self.__getitem__(key)
-        if isinstance(val, dict):
-            val = Result(val)
-            self.__setattr__(key, val)
+    def __getitem__(self, key):
+        self.__set_empty(key)
+        val = super().__getitem__(self.__get_case(key))
         return val
 
-    def __getitem__(self, key):
+    def __getattr__(self, key):
+        return self.__getitem__(key)
+
+    def __setitem__(self, key, val):
+        # we set the item, but if it is a dict we convert the dict to a Result first
+        if isinstance(val, dict):
+            val = Result(val)
+        super().__setitem__(self.__get_case(key), val)
+
+    def __setattr__(self, key, val):
+        self.__setitem__(key, val)
         if key not in self:
             val = Result()
             self.__setitem__(key, val)
