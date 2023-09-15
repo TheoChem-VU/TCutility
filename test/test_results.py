d1ffc3fe4df627e8c96423924d5830da8442c1d0
Yuman Hordijk <yumanhordijk@gmail.com>
2023-09-15 17:11:52 +0200

    Added new test-cases for determining the level-of-theories for various calculations

--- test/test_results.py
+++ test/test_results.py
@@ -14,6 +14,27 @@ def test_dft_engine() -> None:
     assert res.engine == 'adf'
 
 
+def test_LOT() -> None:
+    res = results.read(j('test', 'fixtures', 'DFT_EDA'))
+    assert res.level.summary == 'OLYP/TZ2P'
+
+
+def test_LOT2() -> None:
+    res = results.read(j('test', 'fixtures', 'level_of_theory', 'MP2_SOS'))
+    assert res.level.summary == 'MP2-SOS/TZ2P'
+
+
+def test_LOT3() -> None:
+    res = results.read(j('test', 'fixtures', 'level_of_theory', 'PBE0_MBD_rsSC'))
+    assert res.level.summary == 'PBE0-MBD@rsSC/TZ2P'
+
+
+def test_LOT4() -> None:
+    res = results.read(j('test', 'fixtures', 'level_of_theory', 'M06_2X'))
+    assert res.level.summary == 'M06-2X/QZ4P'
+
+
+
 def test_sections() -> None:
     res = results.read(j('test', 'fixtures', 'DFT_EDA'))
     assert all(section in res for section in ['adf', 'engine', 'ams_version', 'history', 'is_multijob', 'molecule', 'status', 'timing'])
