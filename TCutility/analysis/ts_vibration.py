bad94edcee62db693d1246ccccab9d66a8f40c1f
Koen <koen.salimans@gmail.com>
2023-10-10 15:51:37 +0200

    added documentation

--- TCutility/analysis/ts_vibration.py
+++ TCutility/analysis/ts_vibration.py
@@ -1,25 +1,32 @@
 import numpy as np
 from TCutility.results import read
+from scm import plams
 
-# temp
-from yutility import molecule
-def extract_rc_from_xyz(calc_dir):
-    inputmol = molecule.load(calc_dir + '/input_mol.xyz')
-    return np.array([[idx+1 for idx in molecule.get_labeled_atoms(inputmol['molecule'], 'active_atoms_0', return_idx=True)] + [1]])
-
-def atom_distance(molecule, atom1, atom2):
+def atom_distance(molecule: plams.Molecule, atom1: int, atom2: int) -> float:
     res = 0.0
     for i,j in zip(molecule[atom1],molecule[atom2]):
         res += (i - j)**2
     return np.sqrt(res)
 
-def relative_bond_length_delta(base,pos,neg,a1,a2):
+def avg_relative_bond_length_delta(base: plams.Molecule, pos: plams.Molecule,neg: plams.Molecule, a1: int, a2: int) -> float:
     basedist = atom_distance(base, a1+1, a2+1)
     x = abs((atom_distance(pos, a1+1, a2+1)/basedist)-1)
     y = abs((atom_distance(neg, a1+1, a2+1)/basedist)-1)
     return (x+y)/2
 
-def determine_ts_reactioncoordinate(calc_dir, bond_tolerance=1.28, min_delta_dist=0.1):
+def determine_ts_reactioncoordinate(calc_dir: str, bond_tolerance: float = 1.28, min_delta_dist: float = 0.1) -> np.ndarray:
+    '''Function to retrieve reaction coordinate from a given transitionstate, using the first imaginary frequency.
+
+    Args:
+        calc_dir: path pointing to the desired calculation.
+        bond_tolerance: parameter for plams.molecule.guess_bonds() function
+        min_delta_dist: minimum relative bond length change before qualifying as active atom
+
+    Returns:
+        Array containing all the obtained reaction coordinates. Reaction coordinate format is [active_atom1, active_atom2, sign]
+        Symmetry elements are ignored, by convention the atom labels are increasing (atom1 < atom2)
+    '''
+
     data = read(calc_dir)
     assert 'modes' in data.properties.vibrations, 'Vibrational data is required, but was not present in .rkf file'
 
@@ -39,19 +46,31 @@ def determine_ts_reactioncoordinate(calc_dir, bond_tolerance=1.28, min_delta_dis
     rc = np.array(
                 [   [a+1, b+1, np.sign(posvib.bond_matrix()[a][b] - negvib.bond_matrix()[a][b])]
                     for a, b in np.c_[pairs[0], pairs[1]] 
-                    if a < b and relative_bond_length_delta(outputmol, posvib, negvib, a, b) > min_delta_dist
-                ], dtype=int)
+                    if a < b and avg_relative_bond_length_delta(outputmol, posvib, negvib, a, b) > min_delta_dist
+                ],  dtype=int)
     return rc
 
 
-def validate_transitionstate(calc_dir, rcatoms = None):
-    data = read(calc_dir)
-    result = determine_ts_reactioncoordinate(calc_dir)
+def validate_transitionstate(calc_dir: str, rcatoms = None, bond_tolerance: float = 1.28, min_delta_dist: float = 0.1) -> bool:
+    '''Function to determine whether a transition state calculation yielded the expected transition state.
+
+    Args:
+        calc_dir: path pointing to the desired calculation.
+        rcatoms: list or array containing expected reaction coordinates, to check against the transition state. If not defined, it is obtained from the ams.rkf user input
+        bond_tolerance: parameter for plams.molecule.guess_bonds() function, used in determine_ts_reactioncoordinate
+        min_delta_dist: minimum relative bond length change before qualifying as active atom, used in determine_ts_reactioncoordinate
+
+    Returns:
+        Boolean value, True if the found transition state reaction coordinates match the expected reaction coordinates, False otherwise
+    '''
+
+    result = determine_ts_reactioncoordinate(calc_dir, bond_tolerance, min_delta_dist)
 
     if isinstance(rcatoms, list):
         rcatoms = np.array(rcatoms)
 
     if not isinstance(rcatoms, np.ndarray):
+        data = read(calc_dir)
         assert 'reactioncoordinate' in data.input.transitionstatesearch, 'Reaction coordinate is a required input, but was neither provided nor present in the .rkf file'
         rcatoms = np.array([[int(float(y)) for y in x.split()[1:]] for x in data.input.transitionstatesearch.reactioncoordinate])
 
@@ -69,27 +88,20 @@ def validate_transitionstate(calc_dir, rcatoms = None):
     result = result[result[:,1].argsort()]
     result = result[result[:,0].argsort()]
 
-    # match arbitrary sign with reaction coordinate
+    # only the first reaction coordinate sign is arbitrary, check remaining coordinates for consistency
     result[:, 2] = result[:, 2] if np.sign(result[0][2]) == rcatoms[0][2] else -result[:, 2]
     ret = np.all(result == rcatoms)    
 
     return ret
 
 if __name__ == '__main__':
-    file = '/Users/yumanhordijk/Koen/tcml2022/yuman/ychem/calculations2/cb564ea7b7ec6e28a09bbc474787fe71d8c0406b07a3444ae9e4e0196f3d7a1e/transitionstate'
+    file = '../../test/fixtures/radical_addition_ts'
     print(determine_ts_reactioncoordinate(file))
-    print(extract_rc_from_xyz(file))
     print(validate_transitionstate(file))
-    print(validate_transitionstate(file, extract_rc_from_xyz(file)))
     print(validate_transitionstate(file, [1,16,1]))
     print(validate_transitionstate(file, [[1,16,1]]))
 
     print('\n')
 
-    file = '/Users/yumanhordijk/Koen/temporary_going_to_delete/ts sn2.results'
-    print(determine_ts_reactioncoordinate(file))
-
-    print('\n')
-
-    file = '../../test/fixtures/2'
+    file = '../../test/fixtures/chloromethane_sn2_ts'
     print(determine_ts_reactioncoordinate(file))
