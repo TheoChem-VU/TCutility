e4e094928aba03ff8878ccd4f8b8d7d1f9ed91d5
Koen <koen.salimans@gmail.com>
2023-10-19 12:44:24 +0200

    use `is` instead of `==`

--- test/test_vibrational_ts_analysis.py
+++ test/test_vibrational_ts_analysis.py
@@ -19,36 +19,36 @@ def test_rad_determine_rc_2() -> None:
 
 def test_validate_sn2_ts_1() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'chloromethane_sn2_ts'), [[1,2,1],[1,6,-1]])
-	assert result == True
+	assert result is True
 
 def test_validate_sn2_ts_2() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'chloromethane_sn2_ts'), [[2,3,1],[1,6,-1]]) 
-	assert result == False 	# [2,3,1] should not be present
+	assert result is False 	# [2,3,1] should not be present
 
 def test_validate_rad_ts_1() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts')) 
-	assert result == True
+	assert result is True
 
 def test_validate_rad_ts_2() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [1,16,1], min_delta_dist=0.1)
-	assert result == True 
+	assert result is True 
 
 def test_validate_rad_ts_3() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [[9,8,-3], [16,1,-5]], min_delta_dist=0.0)
-	assert result == True # Testing user input normalization
+	assert result is True # Testing user input normalization
 
 def test_validate_rad_ts_4() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [1,16])
-	assert result == True 
+	assert result is True 
 
 def test_validate_rad_ts_5() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [8,9,1], min_delta_dist=0.1)
-	assert result == False # Should not be present with this min_delta_dist parameter value
+	assert result is False # Should not be present with this min_delta_dist parameter value
 
 def test_validate_rad_ts_6() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [[1,16,1], [8,9,-1]])
-	assert result == False # Should have equal signs
+	assert result is False # Should have equal signs
 
 def test_validate_rad_ts_7() -> None:
 	result = ts_vibration.validate_transitionstate(j('test', 'fixtures', 'radical_addition_ts'), [[1,16], [8,9,1]])
-	assert result == False # Sign has to be provided for all reaction coordinates or none of them
+	assert result is False # Sign has to be provided for all reaction coordinates or none of them
