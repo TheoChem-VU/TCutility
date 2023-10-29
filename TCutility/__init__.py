b31c0fb116a9a0b155ee059d7a90540c3ceb6172
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-10-29 17:18:37 +0100

    Revert "Added ensure_2d, a function used to ensure that a object/list/list-of-lists is always a list-of-lists"
    This reverts commit 13502ced893fa036d057fbc8af2403667464945f.

--- TCutility/__init__.py
+++ TCutility/__init__.py
@@ -1,15 +1,4 @@
 ensure_list = lambda x: [x] if not isinstance(x, (list, tuple, set)) else x  # noqa: E731
 squeeze_list = lambda x: x[0] if len(x) == 1 else x  # noqa: E731
 
-
-def ensure_2d(x, transposed=False):
-    x = ensure_list(x)
-    if transposed:
-        if not isinstance(x[0], (list, tuple, set)):
-            x = [x]
-    else:
-        x = [ensure_list(y) for y in x]
-    return x
-
-
 from TCutility import analysis, results, constants  # noqa: F401, E402
