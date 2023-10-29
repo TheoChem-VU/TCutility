032d83e0c8cda74f1c120f8f7256fd0e8954ca9c
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-10-29 19:10:02 +0100

    Cleaned up test-cases and added new ones for ensure_2d

--- TCutility/__init__.py
+++ TCutility/__init__.py
@@ -1,4 +1,4 @@
-ensure_list = lambda x: [x] if not isinstance(x, (list, tuple, set)) else x  # noqa: E731
+ensure_list = lambda x: [x] if not isinstance(x, (list, tuple, set)) else list(x)  # noqa: E731
 squeeze_list = lambda x: x[0] if len(x) == 1 else x  # noqa: E731
 
 
@@ -6,7 +6,9 @@ def ensure_2d(x, transposed=False):
     x = ensure_list(x)
     if transposed:
         if not isinstance(x[0], (list, tuple, set)):
-            x = [x]
+            x = [ensure_list(x)]
+        else:
+            x = [ensure_list(y) for y in x]
     else:
         x = [ensure_list(y) for y in x]
     return x
