bb9db4b90935f0df7097611ba233b8e3be7f2cea
Yuman Hordijk <yumanhordijk@gmail.com>
2023-11-09 09:29:47 +0100

    Added new test cases and fixed spelling issues

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -6,25 +6,25 @@ def test_rotation_matrix():
 	test_rot = np.array([[-0.3587314, -0.0511359,  0.9320391],
   						 [-0.2293407, -0.9630634, -0.1411088],
   						 [ 0.9048285, -0.2643747,  0.3337536]]).round(5)
-	assert (geometry.get_rotation_matrix(0.4, 1.2, 3).round(5) == test_rot).all()
+	assert (geometry.get_rotmat(0.4, 1.2, 3).round(5) == test_rot).all()
 
 
-def test_apply_rotation_matrix():
-	R = geometry.get_rotation_matrix(y=np.pi)
+def test_apply_rotmat():
+	R = geometry.get_rotmat(y=np.pi)
 	a = np.array([1, 0, 0])
-	assert (geometry.apply_rotation_matrix(a, R).round(5) == np.array([-1, 0, 0])).all()
+	assert (geometry.apply_rotmat(a, R).round(5) == np.array([-1, 0, 0])).all()
 
 
-def test_apply_rotation_matrix2():
-	R = geometry.get_rotation_matrix()
+def test_apply_rotmat2():
+	R = geometry.get_rotmat()
 	a = np.array([1, 0, 0])
-	assert (geometry.apply_rotation_matrix(a, R).round(5) == a).all()
+	assert (geometry.apply_rotmat(a, R).round(5) == a).all()
 
-def test_apply_rotation_matrix3():
-	R = geometry.get_rotation_matrix(y=np.pi)
+def test_apply_rotmat3():
+	R = geometry.get_rotmat(y=np.pi)
 	a = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
 	b = np.array([[-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0]])
-	assert (geometry.apply_rotation_matrix(a, R).round(5) == b).all()
+	assert (geometry.apply_rotmat(a, R).round(5) == b).all()
 
 
 def test_apply_rotate():
@@ -36,10 +36,12 @@ def test_apply_rotate2():
 	a = np.array([1, 0, 0])
 	assert (geometry.rotate(a).round(5) == a).all()
 
+
 def test_apply_rotate3():
 	a = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]])
 	b = np.array([[-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0]])
 	assert (geometry.rotate(a, y=np.pi).round(5) == b).all()
+
 
 def test_vector_align_rotmat():
 	a = np.random.rand(3)*20-10
@@ -50,7 +52,8 @@ def test_vector_align_rotmat():
 	a = a/np.linalg.norm(a)
 	b = b/np.linalg.norm(b)
 
-	assert (geometry.apply_rotation_matrix(a, R).round(5) == b.round(5)).all()
+	assert (geometry.apply_rotmat(a, R).round(5) == b.round(5)).all()
+
 
 def test_vector_align_rotmat2():
 	a = np.array([1, 0, 0])
@@ -58,6 +61,14 @@ def test_vector_align_rotmat2():
 
 	R = geometry.vector_align_rotmat(a, b)
 	assert (R.round(5) == np.eye(3)).all()
+
+
+def test_vector_align_rotmat3():
+	a = np.array([-1, 0, 0])
+	b = np.array([1, 0, 0])
+
+	R = geometry.vector_align_rotmat(a, b)
+	assert (geometry.apply_rotmat(a, R).round(5) == b.round(5)).all()
 
 
 
