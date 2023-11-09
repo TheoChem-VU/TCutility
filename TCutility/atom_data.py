cb37addc3441a6b8a2869e318a27a5ab540bf22c
Yuman Hordijk <yumanhordijk@gmail.com>
2023-11-09 16:05:40 +0100

    Rewrote to use pathlib instead of os

--- TCutility/atom_data.py
+++ TCutility/atom_data.py
@@ -1,24 +1,23 @@
-import os
-
-j = os.path.join
+import pathlib as pl
 
 
 # read data
-data_dir = j(os.path.split(__file__)[0], 'atom_data')
+# data_dir = j(os.path.split(__file__)[0], 'atom_data')
+data_dir = pl.Path(__file__).parents[0] / 'atom_data'
 
-with open(j(data_dir, 'name.txt')) as data:
+with open(data_dir / 'name.txt') as data:
     lines = data.readlines()
 _element_order = [line.split(',')[1].strip() for line in lines]
 
-with open(j(data_dir, 'symbol.txt')) as data:
+with open(data_dir / 'symbol.txt') as data:
     lines = data.readlines()
 _symbol_order = [line.split(',')[1].strip() for line in lines]
 
-with open(j(data_dir, 'radius.txt')) as data:
+with open(data_dir / 'radius.txt') as data:
     lines = data.readlines()
 _radii = {int(line.split(',')[0]): float(line.split(',')[1].strip()) for line in lines}
 
-with open(j(data_dir, 'color.txt')) as data:
+with open(data_dir / 'color.txt') as data:
     lines = data.readlines()
 _colors = {int(line.split(',')[0]): [int(x.strip()) for x in line.split(',')[1:]] for line in lines}
 
