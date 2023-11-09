9e627a3b1c65d442ca52d320f87f025e0ad653a6
Yuman Hordijk <yumanhordijk@gmail.com>
2023-11-09 14:30:05 +0100

    Added atom_data module. Currently holds radius and color of elements, for use in TCviewer

---
+++ TCutility/atom_data.py
@@ -0,0 +1,47 @@
+import periodictable as pt
+import os
+
+j = os.path.join
+
+
+# read data
+data_dir = j(os.path.split(__file__)[0], 'atom_data')
+
+with open(j(data_dir, 'name.txt')) as data:
+    lines = data.readlines()
+_element_order = [line.split(',')[1].strip() for line in lines]
+
+with open(j(data_dir, 'symbol.txt')) as data:
+    lines = data.readlines()
+_symbol_order = [line.split(',')[1].strip() for line in lines]
+
+with open(j(data_dir, 'radius.txt')) as data:
+    lines = data.readlines()
+_radii = {int(line.split(',')[0]): float(line.split(',')[1].strip()) for line in lines}
+
+with open(j(data_dir, 'color.txt')) as data:
+    lines = data.readlines()
+_colors = {int(line.split(',')[0]): [int(x.strip()) for x in line.split(',')[1:]] for line in lines}
+
+
+def parse_element(val):
+    if isinstance(val, int):
+        return val
+    if val.lower() in _element_order:
+        return _element_order.index(val.lower()) + 1
+    if val in _symbol_order:
+        return _symbol_order.index(val) + 1
+
+
+def radius(element):
+    num = parse_element(element)
+    return radii.get(num)
+
+
+def color(element):
+    num = parse_element(element)
+    return _colors[num]
+
+
+if __name__ == '__main__':
+    print(color('carbon'))
