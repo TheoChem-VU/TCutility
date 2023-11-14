87b5f1443395fff449e6fc8d93e8dd996e3fba02
Yuman Hordijk <yumanhordijk@gmail.com>
2023-11-14 18:30:15 +0100

    Fixed support for LaTeX parsing, which was different from HTML parsing

--- TCutility/formula.py
+++ TCutility/formula.py
@@ -9,38 +9,34 @@ def molecule(molstring: str, mode: str = 'html') -> str:
 	Returns:
 		A string that is formatted to be rendered nicely in either HTML or LaTeX.
 	'''
-	if mode == 'latex':
-		ret = molstring
+	# to take care of plus-signs used to denote reactions we have to first split 
+	# the molstring into its parts.
+	for part in molstring.split():
+		# if part is only a plus-sign we skip this part. This is only true when the plus-sign
+		# is used to denote a reaction
+		if part in ['+', '->']:
+			continue
+
+		# parse the part
+		partret = part
+		# numbers should be subscript
 		for num in '0123456789':
-			ret = ret.replace(num, f'_{num}')
-		for sign in '+-':
-			ret = ret.replace(sign, f'^{sign}')
-		return ret
-
-	if mode == 'html':
-		ret = molstring
-		# to take care of plus-signs used to denote reactions we have to first split 
-		# the molstring into its parts.
-		for part in ret.split():
-			# if part is only a plus-sign we skip this part. This is only true when the plus-sign
-			# is used to denote a reaction
-			if part in ['+', '->']:
-				continue
-
-			# parse the part
-			partret = part
-			# numbers should be subscript
-			for num in '0123456789':
+			if mode == 'latex':
+				partret = partret.replace(num, f'_{num}')
+			if mode == 'html':
 				partret = partret.replace(num, f'<sub>{num}</sub>')
 
-			# signs should be superscript
-			for sign in '+-':
-				# negative charges should be denoted by em dash and not a normal dash
+		# signs should be superscript
+		for sign in '+-':
+			# negative charges should be denoted by em dash and not a normal dash
+			if mode == 'latex':
+				partret = partret.replace(sign, f'^{sign.replace("-", "—")}')
+			if mode == 'html':
 				partret = partret.replace(sign, f'<sup>{sign.replace("-", "—")}</sup>')
+		# replace the part in the original string
+		molstring = molstring.replace(part, partret)
 
-			# replace the part in the original string
-			ret = ret.replace(part, partret)
-		return ret
+	return molstring
 
 
 if __name__ == '__main__':
