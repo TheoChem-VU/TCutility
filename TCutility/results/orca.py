95da442bd337e692a4a75458ac3bf60e3557a7e2
Yuman Hordijk <yumanhordijk@gmail.com>
2023-12-11 16:27:39 +0100

    Added documentation to all functions

--- TCutility/results/orca.py
+++ TCutility/results/orca.py
@@ -206,6 +206,19 @@ def get_calc_settings(info: Result) -> Result:
 
 
 def get_calculation_status(info: Result) -> Result:
+    '''Function that returns the status of the ORCA calculation described in reader. In case of non-succes it will also give possible reasons for the errors/warnings.
+
+    Args:
+        info: Result object containing ORCA calculation information.
+
+    Returns:
+        :Result object containing information about the calculation status:
+
+            - **fatal (bool)** – True if calculation cannot be analysed correctly, False otherwise
+            - **reasons (list[str])** – list of reasons to explain the status, they can be errors, warnings, etc.
+            - **name (str)** – calculation status written as a string, one of ("SUCCESS", "RUNNING", "UNKNOWN", "SUCCESS(W)", "FAILED")
+            - **code (str)** – calculation status written as a single character, one of ("S", "R", "U", "W" "F")
+    '''
     ret = Result()
     ret.fatal = True
     ret.name = None
@@ -232,11 +245,21 @@ def get_calculation_status(info: Result) -> Result:
 
 
 def get_molecules(info: Result) -> Result:
+    '''Function that returns information about the molecules for this calculation.
+
+    Args:
+        info: Result object containing ORCA calculation information.
+
+    Returns:
+        :Result object containing properties from the ORCA calculation:
+
+            - **input (plams.Molecule)** - the input molecule for this calculation.
+            - **output (plams.Molecule)** - the output molecule for this calculation, for example the optimized structure after a geometry optimization.
+            - **number_of_atoms (int)** - the number of atoms in the molecular system.
+    '''
     ret = Result()
     ret.input = info.input.system.molecule
     ret.number_of_atoms = len(ret.input.atoms)
-
-    ret.output = None
 
     with open(info.files.out) as out:
         lines = out.readlines()
@@ -278,6 +301,9 @@ def get_info(calc_dir: str) -> Result:
         :Dictionary containing results about the calculation and AMS:
 
             - **version (Result)** – information about the AMS version used, see :func:`get_version`.
+            - **files (Result)** - paths to files that are important for this calculation.
+            - **input (Result)** - information about the input of this calculation, see :func:`get_input`.
+            - **level (Result)** - information about the level of theory used for this calculation, see :func:`get_level_of_theory`.
             - **engine (str)** – the engine that was used to perform the calculation, for example 'adf', 'dftb', ...
             - **status (Result)** – information about calculation status, see :func:`get_calculation_status`.
             - **molecule (Result)** – information about the input and output molecules and the molecular system in general, see :func:`get_molecules`.
@@ -305,6 +331,21 @@ def get_info(calc_dir: str) -> Result:
 
 
 def get_vibrations(lines):
+    '''Function to read vibrational data of an ORCA calculation.
+
+    Args:
+        lines: Lines in the output file of the ORCA calculation.
+
+    Returns:
+        :Result object containing vibrational properties from the ORCA calculation:
+
+            - **number_of_modes (int)** – number of vibrational modes for this molecule, 3N-5 for non-linear molecules and 3N-6 for linear molecules, where N is the number of atoms.
+            - **number_of_imaginary_modes (int)** – number of imaginary vibrational modes for this molecule.
+            - **frequencies (list[float])** – vibrational frequencies associated with the vibrational modes, sorted from low to high (|cm-1|).
+            - **intensities (list[float])** – vibrational intensities associated with the vibrational modes (|km/mol|). In ORCA, intensities of imaginary modes are set to 0.
+            - **modes (list[float])** – list of vibrational modes sorted from low frequency to high frequency.
+            - **character (str)** – the PES character of the molecular system. Either "minimum", "transitionstate" or "saddlepoint(n_imag)", for 0, 1, n_imag number of imaginary frequencies.
+    '''
     ret = Result()
     start_reading = False
     start_reading_idx = 0
@@ -320,12 +361,12 @@ def get_vibrations(lines):
             start_reading = True
             start_reading_idx = i
 
-    nmodes = len(freq_lines)
+    ret.number_of_modes = len(freq_lines)
     frequencies = [float(line.split()[1]) for line in freq_lines]
     nrotranslational = sum([freq == 0 for freq in frequencies])
     ret.frequencies = frequencies[nrotranslational:]
-    ret.number_of_imag_modes = len([freq for freq in ret.frequencies if freq < 0])
-    ret.character = 'minimum' if ret.number_of_imag_modes == 0 else 'transitionstate' if ret.number_of_imag_modes == 1 else f'saddlepoint({ret.number_of_imag_modes})'
+    ret.number_of_imaginary_modes = len([freq for freq in ret.frequencies if freq < 0])
+    ret.character = 'minimum' if ret.number_of_imaginary_modes == 0 else 'transitionstate' if ret.number_of_imaginary_modes == 1 else f'saddlepoint({ret.number_of_imaginary_modes})'
 
     start_reading = False
     mode_lines = []
@@ -342,12 +383,12 @@ def get_vibrations(lines):
             mode_lines.append(line)
 
     mode_lines = mode_lines[6:-3]
-    mode_lines = [[float(x) for x in line.split()[1:]] for i, line in enumerate(mode_lines) if i % (nmodes + 1) != 0]
+    mode_lines = [[float(x) for x in line.split()[1:]] for i, line in enumerate(mode_lines) if i % (ret.number_of_modes + 1) != 0]
 
-    nblocks = len(mode_lines)//nmodes
+    nblocks = len(mode_lines)//ret.number_of_modes
     blocks = []
     for block in range(nblocks):
-        blocks.append(np.array(mode_lines[block * nmodes: (block + 1) * nmodes]))
+        blocks.append(np.array(mode_lines[block * ret.number_of_modes: (block + 1) * ret.number_of_modes]))
     ret.modes = np.hstack(blocks).T.tolist()[nrotranslational:]
 
     start_reading = False
@@ -365,11 +406,36 @@ def get_vibrations(lines):
             int_lines.append(line)
 
     ints = [float(line.split()[3]) for line in int_lines[5:-1]]
-    ret.intensities = [0] * ret.number_of_imag_modes + ints
+    ret.intensities = [0] * ret.number_of_imaginary_modes + ints
     return ret
 
 
 def get_properties(info: Result) -> Result:
+    '''Function to get properties from an ORCA calculation.
+
+    Args:
+        info: Result object containing ORCA properties.
+
+    Returns:
+        :Result object containing properties from the ORCA calculation:
+
+            - **energy.bond (float)** – total bonding energy (|kcal/mol|).
+            - **energy.enthalpy (float)** – enthalpy (|kcal/mol|). Only obtained if vibrational modes were calculated.
+            - **energy.entropy (float)** – entropy (|kcal/mol|). Only obtained if vibrational modes were calculated.
+            - **energy.gibbs (float)** – Gibb's free energy (|kcal/mol|). Only obtained if vibrational modes were calculated.
+            - **energy.[method] (float)** - total energy (|kcal/mol|) at a certain level (HF, MP2, CCSD, ...). This is the sum of energy.HF and energy.[method]_corr.
+            - **energy.[method]_corr (float)** - electron correlation energy (|kcal/mol|) at a certain level (HF, MP2, CCSD, ...).
+            - **vibrations.number_of_modes (int)** – number of vibrational modes for this molecule, 3N-5 for non-linear molecules and 3N-6 for linear molecules, where N is the number of atoms.
+            - **vibrations.number_of_imaginary_modes (int)** – number of imaginary vibrational modes for this molecule.
+            - **vibrations.frequencies (list[float])** – vibrational frequencies associated with the vibrational modes, sorted from low to high (|cm-1|).
+            - **vibrations.intensities (list[float])** – vibrational intensities associated with the vibrational modes (|km/mol|). In ORCA, intensities of imaginary modes are set to 0.
+            - **vibrations.modes (list[float])** – list of vibrational modes sorted from low frequency to high frequency.
+            - **vibrations.character (str)** – the PES character of the molecular system. Either "minimum", "transitionstate" or "saddlepoint(n_imag)", for 0, 1, n_imag number of imaginary frequencies.
+            - **t1** - T1 diagnostic for the highest level of correlation chosen. Used to check the validity of the reference wavefunction.
+            - **s2** - expectation value of the :math:`S^2` operator.
+            - **s2_expected** - ideal expectation value of the :math:`S^2` operator.
+            - **spin_contamination** - the amount of spin-contamination observed in this calculation. It is equal to (s2 - s2_expected) / (s2_expected). Ideally this value should be below 0.1.
+    '''
     ret = Result()
 
     with open(info.files.out) as out:
@@ -378,6 +444,7 @@ def get_properties(info: Result) -> Result:
     if info.orca.frequencies:
         ret.vibrations = get_vibrations(lines)
 
+    # read some important info about the calculation
     for line in lines:
         if 'FINAL SINGLE POINT ENERGY' in line:
             ret.energy.bond = float(line.split()[4]) * constants.HA2KCALMOL
@@ -393,14 +460,17 @@ def get_properties(info: Result) -> Result:
 
         if 'E(MP2)' in line:
             ret.energy.MP2 = float(line.split()[-1]) * constants.HA2KCALMOL + ret.energy.HF
+            ret.energy.MP2_corr = float(line.split()[-1]) * constants.HA2KCALMOL
             continue
 
         if 'E(CCSD) ' in line:
             ret.energy.CCSD = float(line.split()[-1]) * constants.HA2KCALMOL
+            ret.energy.CCSD_corr = float(line.split()[-1]) * constants.HA2KCALMOL - ret.energy.HF
             continue
 
         if 'E(CCSD(T))' in line:
             ret.energy.CCSD_T = float(line.split()[-1]) * constants.HA2KCALMOL
+            ret.energy.CCSD_T_corr = float(line.split()[-1]) * constants.HA2KCALMOL - ret.energy.HF
             continue
 
         if 'Final Gibbs free energy' in line:
