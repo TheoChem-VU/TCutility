0885236d870aff6abb06bd367b9e6cb4c12236a4
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-15 14:33:13 +0100

    Debugging errors in detecting slurm status

--- src/tcutility/results/__init__.py
+++ src/tcutility/results/__init__.py
@@ -31,6 +31,8 @@ def get_info(calc_dir: str):
         pass
 
     res = Result()
+
+    print(calc_dir, slurm.workdir_info(calc_dir), slurm.squeue())
     # if we cannot correctly read the info, we return some generic result object
     # before that, we check the job status by checking slurm
     if slurm.workdir_info(calc_dir):
