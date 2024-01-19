f6d1d5de175824fc94a0bd4b6efdd495c003cc99
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-19 21:31:12 +0100

    Added kill_on_invalid_dep when setting a dependency in case the previous job fails

--- src/tcutility/job/generic.py
+++ src/tcutility/job/generic.py
@@ -129,6 +129,7 @@ class Job:
         '''
         if hasattr(otherjob, 'slurm_job_id'):
             self.sbatch(dependency=f'afterok:{otherjob.slurm_job_id}')
+            self.sbatch(kill_on_invalid_dep='Yes')
 
     @property
     def workdir(self):
