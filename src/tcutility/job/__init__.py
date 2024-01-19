57723c8ee14bff9ff9dcbf4118a92c40d2eb96f9
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-19 21:22:00 +0100

    Added crest-job to init

--- src/tcutility/job/__init__.py
+++ src/tcutility/job/__init__.py
@@ -2,3 +2,4 @@ from .generic import Job  # noqa: F401
 from .ams import AMSJob  # noqa: F401
 from .adf import ADFJob, ADFFragmentJob  # noqa: F401
 from .dftb import DFTBJob  # noqa: F401
+from .crest import CRESTJob  # noqa: F401
