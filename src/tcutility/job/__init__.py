3ac36c66ee43d292277598c142658276d4f98490
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-20 11:57:32 +0100

    Importing orcajob

--- src/tcutility/job/__init__.py
+++ src/tcutility/job/__init__.py
@@ -3,3 +3,4 @@ from .ams import AMSJob  # noqa: F401
 from .adf import ADFJob, ADFFragmentJob  # noqa: F401
 from .dftb import DFTBJob  # noqa: F401
 from .crest import CRESTJob  # noqa: F401
+from .orca import ORCAJob  # noqa: F401
