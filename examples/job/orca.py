eda7edb3fef6318c1c9db3de2a1eef1acbbcfbfc
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-20 12:08:05 +0100

    Improved example

--- examples/job/orca.py
+++ examples/job/orca.py
@@ -2,4 +2,7 @@ from tcutility.job import ORCAJob
 
 with ORCAJob() as job:
     job.molecule('water.xyz')
-    job.sbatch(p='tc', n=32)
+    job.sbatch(p='tc', n=32, mem=20000)
+    job.name = 'ORCA_opt'
+    job.rundir = 'calculations/water'
+    job.optimization()
