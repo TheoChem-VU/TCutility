13f0d6283e28a1a9f28d0df70cedf6d5dcf60ed3
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-22 21:37:18 +0100

    Prettified the example

--- examples/job/screen_functionals.py
+++ examples/job/screen_functionals.py
@@ -1,14 +1,23 @@
 from tcutility.job import ADFJob
 from tcutility.data import functionals
 
-for func_name, func_data in functionals.get_available_functionals().items():
+available_functionals = functionals.get_available_functionals()
+for functional_name, functional_info in available_functionals.items():
     with ADFJob(test_mode=True) as job:
+        # load your ams version
         job.add_preamble('module load ams/2023.101')
+        # load your molecule
         job.molecule('molecule.xyz')
-        job.rundir = f'DFT_screening/{func_data.category}'
-        job.name = func_data.path_safe_name
+
+        # sort calculation by their category
+        job.rundir = f'DFT_screening/{functional_info.category}'
+        job.name = functional_info.path_safe_name
+
+        # slurm settings
         job.sbatch(p='tc', ntasks_per_node=32)
-        job.functional(func_name)
+
+        # ADF settings
+        job.functional(functional_name)
         job.basis_set('TZ2P')
 
 
