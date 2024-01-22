f6dfd3d65a28b526b3eabe9ae1eb77047d1006b8
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-22 21:38:12 +0100

    Added a get_molecules function which returns molecules from the molecules directory. Optionally you can give tags that have to be defined in the xyz file

--- src/tcutility/data/molecules.py
+++ src/tcutility/data/molecules.py
@@ -6,18 +6,19 @@ j = os.path.join
 
 root_dir = j(os.path.split(__file__)[0], 'molecules')
 
-def get_mol(name):
+def get(name):
 	p = j(root_dir, f'{name.removesuffix(".xyz")}.xyz')
 	return molecule.load(p)
 
 
-def get_available_molecules():
+def get_molecules(tags=None):
 	for f in os.listdir(root_dir):
 		if f == '.DS_Store':
 			continue
-
-		yield get_mol(f)
+		mol = get(f)
+		if tags is None or any(tag in mol.flags.tags for tag in tags):
+			yield mol
 
 if __name__ == '__main__':
-	for mol in get_available_molecules():
-		print(mol.flags.tags)
+	for mol in get_molecules():
+		print(mol)
