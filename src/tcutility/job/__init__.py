96810f0e82e25f09ef40b26c6687023b38807685
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-22 15:06:08 +0100

    Added the QCGJob to init

--- src/tcutility/job/__init__.py
+++ src/tcutility/job/__init__.py
@@ -2,5 +2,5 @@ from .generic import Job  # noqa: F401
 from .ams import AMSJob  # noqa: F401
 from .adf import ADFJob, ADFFragmentJob  # noqa: F401
 from .dftb import DFTBJob  # noqa: F401
-from .crest import CRESTJob  # noqa: F401
+from .crest import CRESTJob, QCGJob  # noqa: F401
 from .orca import ORCAJob  # noqa: F401
