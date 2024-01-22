aaf77fc6c9a738bec4202d94f07647d072180fbd
YHordijk <42876712+YHordijk@users.noreply.github.com>
2024-01-22 23:07:35 +0100

    Changed tabs to spaces

--- src/tcutility/data/molecules.py
+++ src/tcutility/data/molecules.py
@@ -6,19 +6,21 @@ j = os.path.join
 
 root_dir = j(os.path.split(__file__)[0], 'molecules')
 
+
 def get(name):
-	p = j(root_dir, f'{name.removesuffix(".xyz")}.xyz')
-	return molecule.load(p)
+    p = j(root_dir, f'{name.removesuffix(".xyz")}.xyz')
+    return molecule.load(p)
 
 
 def get_molecules(tags=None):
-	for f in os.listdir(root_dir):
-		if f == '.DS_Store':
-			continue
-		mol = get(f)
-		if tags is None or any(tag in mol.flags.tags for tag in tags):
-			yield mol
+    for f in os.listdir(root_dir):
+        if f == '.DS_Store':
+            continue
+        mol = get(f)
+        if tags is None or any(tag in mol.flags.tags for tag in tags):
+            yield mol
+
 
 if __name__ == '__main__':
-	for mol in get_molecules():
-		print(mol)
+    for mol in get_molecules():
+        print(mol)
