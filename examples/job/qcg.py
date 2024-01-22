6a2557645fcd607d758350db917de611f04f8c54
YHordijk <42876712+YHordijk@users.noreply.github.com>
2024-01-22 23:38:12 +0100

    Updated the example

--- examples/job/qcg.py
+++ examples/job/qcg.py
@@ -1,8 +1,12 @@
 from tcutility.job import QCGJob
 
 with QCGJob() as job:
-	job.molecule('pentane.xyz')
-	job.solvent('water')
-	job.rundir = 'calculations/pentane'
-	job.name = 'QCG'
-	job.crest_path = 'crest'
+    job.molecule('asc.xyz')
+    job.solvent('dmso.xyz')
+    job.rundir = 'calculations/ASC'
+    job.name = 'QCG'
+    job.md_length(5)
+    job.md_temperature(1000)
+    job.crest_path = 'crest'
+    job.sbatch(p='tc', n=32)
+    job.ensemble_mode('MTD')
