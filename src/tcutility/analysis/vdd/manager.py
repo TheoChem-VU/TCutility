9fc3f3cd0a7eb5af3ab6174f8e530b1bfb1fb4cf
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-01-27 12:44:40 +0100

    Changed methods following the comments from pull request #86

--- src/tcutility/analysis/vdd/manager.py
+++ src/tcutility/analysis/vdd/manager.py
@@ -1,18 +1,21 @@
 ﻿from __future__ import annotations
-import pandas as pd
-import numpy as np
-from typing import Dict, List, Optional, Sequence, Union
-import attrs
-import pathlib as pl
-from itertools import zip_longest
+
 import copy
 import functools
-from tcutility.analysis.vdd import charge
-from tcutility.results import result
+import pathlib as pl
+from itertools import zip_longest
+from typing import Dict, List, Optional, Sequence, Union
+
+import attrs
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+import tcutility.log as log
+from tcutility.analysis.vdd import charge
+from tcutility.constants import VDD_UNITS
+from tcutility.results import result
 
-AVAILABLE_UNITS_RATIO = {"me": 1000, "e": 1}
 PRINT_FORMAT = {"me": "%+.0f", "e": "%+.3f"}
 
 
@@ -28,11 +31,14 @@ def change_unit_decorator(func):
     return wrapper
 
 
-def create_vdd_charge_manager(results: result.Result, name: str = "VDDChargeManager") -> VDDChargeManager:
+def create_vdd_charge_manager(results: result.Result) -> VDDChargeManager:
     """Create a VDDChargeManager from a Result object."""
     vdd_charges: Dict[str, List[charge.VDDCharge]] = {}
     atom_symbols = results.molecule.atom_symbols  # type: ignore
     frag_indices = results.molecule.frag_indices  # type: ignore
+    calc_dir = pl.Path(results.files["root"])  # type: ignore
+    is_fragment_calculation = results.adf.used_regions  # type: ignore
+    mol_charge = results.molecule.mol_charge  # type: ignore
 
     # Convert the VDD charges to VDDCharge objects
     for irrep, charge_array in results.properties.vdd.items():  # type: ignore
@@ -42,7 +48,7 @@ def create_vdd_charge_manager(results: result.Result, name: str = "VDDChargeMana
             atom_symbol = atom_symbols[atom_index]
             vdd_charges[irrep].append(charge.VDDCharge(atom_index + 1, atom_symbol, vdd_charge, frag_index))
 
-    return VDDChargeManager(vdd_charges, name)
+    return VDDChargeManager(vdd_charges, is_fragment_calculation, calc_dir, mol_charge)
 
 
 @attrs.define
@@ -50,18 +56,23 @@ class VDDChargeManager:
     """Class to manage the VDD charges. It can be used to print the VDD charges in a nice table and write them to a text file or excel file."""
 
     vdd_charges: Dict[str, List[charge.VDDCharge]]  # {"vdd": [VDDCharge, ...], "irrep1": [VDDCharge, ...], ...}
-    name: str = "VDDChargeManager"  # name of the VDDChargeManager
+    is_fragment_calculation: bool
+    calc_dir: pl.Path
+    mol_charge: int
+    name: str = attrs.field(init=False)
     unit: str = attrs.field(init=False, default="e")  # unit of the VDD charges. Available units are "me" (mili-electrons) and "e" (electrons)
     irreps: List[str] = attrs.field(init=False, default=set())
 
     def __attrs_post_init__(self):
         self.irreps = list(self.vdd_charges.keys())
+        self.name = self.calc_dir.name if self.calc_dir is not None else self.name
         self.change_unit("me")
+        log.info(f"VDDChargeManager created for {self.name}")
 
     def __str__(self) -> str:
         """Prints the VDD charges in a nice table. Checks if the calculation is a fragment calculation and prints the summed VDD charges if it is."""
-        individual_charges_table = self.get_vdd_charges_table(self.unit)
-        summed_charges_table = self.get_summed_vdd_charges_table(self.unit)
+        individual_charges_table = self.get_vdd_charges_table()
+        summed_charges_table = self.get_summed_vdd_charges_table()
 
         ret_str = f"{self.name}\nVDD charges (in unit {self.unit}):\n{individual_charges_table}\n\n"
         if self.is_fragment_calculation:
@@ -69,36 +80,31 @@ class VDDChargeManager:
 
         return ret_str
 
-    @property
-    def is_fragment_calculation(self) -> bool:
-        """Check if the calculation is a fragment calculation by checking if the highest fragment index is equal to the number of atoms."""
-        return max([charge.frag_index for charges in self.vdd_charges.values() for charge in charges]) != len(self.vdd_charges["vdd"])
-
-    def charge_is_conserved(self, mol_charge: int) -> bool:
+    def charge_is_conserved(self) -> bool:
         """Check if the total charge of the molecule is conserved. The total charge is the sum of the VDD charges."""
         tolerance = 1e-4 if self.unit == "e" else 1e-1
-        is_conserved = np.isclose(mol_charge, sum([charge.charge for charge in self.vdd_charges["vdd"]]), atol=tolerance)
+        is_conserved = np.isclose(self.mol_charge, sum([charge.charge for charge in self.vdd_charges["vdd"]]), atol=tolerance)
         return is_conserved  # type: ignore since numpy _bool is not recognized as bool
 
     def change_unit(self, new_unit: str) -> None:
         """Change the unit of the VDD charges. Available units are "me" (mili-electrons) and "e" (electrons)."""
-        if new_unit not in AVAILABLE_UNITS_RATIO:
-            raise ValueError(f"Unit {new_unit} is not available. Choose from {AVAILABLE_UNITS_RATIO.keys()}")
+        if new_unit not in VDD_UNITS:
+            raise ValueError(f"Unit {new_unit} is not available. Choose from {VDD_UNITS.keys()}")
 
         if new_unit == self.unit:
             return
 
-        ratio = AVAILABLE_UNITS_RATIO[new_unit] / AVAILABLE_UNITS_RATIO[self.unit]
+        ratio = VDD_UNITS[new_unit] / VDD_UNITS[self.unit]
         [charge.change_unit(ratio) for charges in self.vdd_charges.values() for charge in charges]
         self.unit = new_unit
 
     @change_unit_decorator
-    def get_vdd_charges(self, unit: str = "me") -> Dict[str, List[charge.VDDCharge]]:
+    def get_vdd_charges(self) -> Dict[str, List[charge.VDDCharge]]:
         """Get the VDD charges in the specified unit ([me] or [e])."""
         return copy.deepcopy(self.vdd_charges)
 
     @change_unit_decorator
-    def get_summed_vdd_charges(self, unit: str = "me", irreps: Optional[Sequence[str]] = None) -> Dict[str, Dict[str, float]]:
+    def get_summed_vdd_charges(self, irreps: Optional[Sequence[str]] = None) -> Dict[str, Dict[str, float]]:
         """Get the summed VDD charges per fragment for the specified unit ([me] or [e])."""
         irreps = irreps if irreps is not None else self.irreps
         summed_vdd_charges: Dict[str, Dict[str, float]] = {}
@@ -112,9 +118,8 @@ class VDDChargeManager:
 
         return copy.deepcopy(summed_vdd_charges)
 
-    def get_vdd_charges_dataframe(self, unit: str = "me") -> pd.DataFrame:
+    def get_vdd_charges_dataframe(self) -> pd.DataFrame:
         """Get the VDD charges as a pandas DataFrame in a specified unit ([me] or [e])."""
-        self.change_unit(unit)
         frag_indices = [charge.frag_index for charge in self.vdd_charges["vdd"]]
         atom_symbols = [f"{charge.atom_index}{charge.atom_symbol}" for charge in self.vdd_charges["vdd"]]
         charges = [[charge.charge for charge in charges] for _, charges in self.vdd_charges.items()]
@@ -123,19 +128,19 @@ class VDDChargeManager:
         df = pd.DataFrame(combined_table, columns=headers).rename(columns={"vdd": "Total"})
         return df
 
-    def get_summed_vdd_charges_dataframe(self, unit: str = "me") -> pd.DataFrame:
+    def get_summed_vdd_charges_dataframe(self) -> pd.DataFrame:
         """Get the summed VDD charges as a pandas DataFrame in a specified unit ([me] or [e])."""
-        summed_data = self.get_summed_vdd_charges(unit)
+        summed_data = self.get_summed_vdd_charges()
         summed_data["Frag"] = {str(key): int(key) for key in summed_data["vdd"].keys()}
         df = pd.DataFrame(summed_data).pipe(lambda df: df[df.columns.tolist()[-1:] + df.columns.tolist()[:-1]])  # move the "Frag" column to the front
         return df.rename(columns={"vdd": "Total"})
 
-    def get_vdd_charges_table(self, unit: str = "me") -> str:
-        df = self.get_vdd_charges_dataframe(unit)
+    def get_vdd_charges_table(self) -> str:
+        df = self.get_vdd_charges_dataframe()
         return df.to_string(float_format=lambda x: PRINT_FORMAT[self.unit] % x, justify="center", index=False, col_space=6)
 
-    def get_summed_vdd_charges_table(self, unit: str = "me") -> str:
-        df = self.get_summed_vdd_charges_dataframe(unit)
+    def get_summed_vdd_charges_table(self) -> str:
+        df = self.get_summed_vdd_charges_dataframe()
         return df.to_string(float_format=lambda x: PRINT_FORMAT[self.unit] % x, justify="center", col_space=6, index=False)
 
     @staticmethod
@@ -148,29 +153,32 @@ class VDDChargeManager:
         # Print charges per atom and per fragment in seperate files
         for i, file in enumerate(files):
             with open(file, "w") as f:
-                f.write(f"VDD charges (in unit {unit}):\n")
+                f.write("VDD charges:\n")
                 for manager in managers:
-                    f.write(f"{manager.name}\n")
+                    f.write(f"{manager.name} (unit = {manager.unit})\n")
                     if i == 0:
-                        f.write(manager.get_vdd_charges_table(unit))
+                        f.write(manager.get_vdd_charges_table())
                     else:
-                        f.write(manager.get_summed_vdd_charges_table(unit)) if manager.is_fragment_calculation else f.write("No fragment calculation\n")
+                        f.write(manager.get_summed_vdd_charges_table()) if manager.is_fragment_calculation else f.write("No fragment calculation\n")
                     f.write("\n\n")
 
-    def write_to_excel(self, output_dir: Union[str, pl.Path], unit: str = "me") -> None:
+    def write_to_excel(self, output_file: Optional[Union[str, pl.Path]] = None) -> None:
         """Write the VDD charges to an excel file. Results are written to two sheets: "VDD charges" and "Summed VDD charges"."""
-        file = pl.Path(output_dir) / f"vdd_charges_{self.name}.xlsx"
-        df = self.get_vdd_charges_dataframe(unit)
+        file = pl.Path(output_file) if output_file is not None else self.calc_dir / f"vdd_charges_{self.name}.xlsx"
+        file = file.with_suffix(".xlsx") if not file.suffix == ".xlsx" else file
+
+        df = self.get_vdd_charges_dataframe()
 
         with pd.ExcelWriter(file) as writer:
-            df.to_excel(writer, sheet_name=f"VDD charges (in {unit})", index=False, float_format=PRINT_FORMAT[self.unit])
+            df.to_excel(writer, sheet_name=f"VDD charges (in {self.unit})", index=False, float_format=PRINT_FORMAT[self.unit])
             if self.is_fragment_calculation:
-                df_summed = self.get_summed_vdd_charges_dataframe(unit)
-                df_summed.to_excel(writer, sheet_name=f"Summed VDD charges  (in {unit})", index=False, float_format=PRINT_FORMAT[self.unit])
+                df_summed = self.get_summed_vdd_charges_dataframe()
+                df_summed.to_excel(writer, sheet_name=f"Summed VDD charges  (in {self.unit})", index=False, float_format=PRINT_FORMAT[self.unit])
 
-    def plot_vdd_charges_per_atom(self, output_dir: Union[str, pl.Path], unit: str = "me") -> None:
+    def plot_vdd_charges_per_atom(self, output_file: Optional[Union[str, pl.Path]] = None, unit: str = "me") -> None:
         """Plot the VDD charges as a bar graph for each irrep."""
-        file = pl.Path(output_dir) / f"vdd_charges_{self.name}.png"
+        file = pl.Path(output_file) if output_file is not None else self.calc_dir / f"vdd_charges_{self.name}.png"
+        file = file.with_suffix(".png") if not file.suffix == ".png" else file
         self.change_unit(unit)
 
         # Increase the global font size
@@ -178,7 +186,7 @@ class VDDChargeManager:
 
         num_irreps = len(self.vdd_charges)
         n_max_charges = max([len(charges) for charges in self.vdd_charges.values()])
-        fig, axs = plt.subplots(num_irreps, 1, figsize=(n_max_charges * 1.15, 5 * num_irreps), sharey=True)
+        _, axs = plt.subplots(num_irreps, 1, figsize=(n_max_charges * 1.15, 5 * num_irreps), sharey=True)
         axs = [axs] if num_irreps == 1 else axs
 
         # Initialize a variable to keep track of the most positive/negative values
