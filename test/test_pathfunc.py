496cf67861a7b9bb29aca83ebc185904d7e90c97
Yuman Hordijk <yumanhordijk@gmail.com>
2024-02-07 14:02:23 +0100

    Added some test-cases for split_all

---
+++ test/test_pathfunc.py
@@ -0,0 +1,29 @@
+from tcutility import pathfunc
+
+
+def test_split_all():
+	assert pathfunc.split_all('a/b/c') == ['a', 'b', 'c']
+
+
+def test_split_all2():
+	assert pathfunc.split_all('a') == ['a']
+
+
+def test_split_all3():
+	assert pathfunc.split_all('') == ['']
+
+
+def test_split_all4():
+	import os
+	assert pathfunc.split_all(os.path.join('a', 'b', 'c')) == ['a', 'b', 'c']
+
+
+def test_split_all4():
+	import os
+	assert pathfunc.split_all(os.path.join('a', 'b', 'c')) == ['a', 'b', 'c']
+
+
+if __name__ == '__main__':
+	import pytest 
+
+	pytest.main()
