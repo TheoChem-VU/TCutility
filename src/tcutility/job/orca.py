f64dab8219e64371972d929bc93d6cd92feba4a5
YHordijk <42876712+YHordijk@users.noreply.github.com>
2024-02-11 22:44:13 +0100

    Fixed a typo in the reference method

--- src/tcutility/job/orca.py
+++ src/tcutility/job/orca.py
@@ -36,8 +36,8 @@ class ORCAJob(Job):
 
     def reference(self, ref):
         spell_check.check(ref, ['UHF', 'UKS', 'RHF', 'RKS', 'ROHF', 'ROKS'])
-        self.settings.main.append(method)
-        self._method = method
+        self.settings.main.append(ref)
+        self._method = ref
 
     def QRO(self, enable=True):
         self.settings.MDCI.UseQROs = enable
