987868be3549a46ead85f99db9b0bbfb37b6643e
SiebeLeDe <siebbe@hotmail.com>
2024-02-28 19:38:17 +0100

    Added an additional script that prints the multikeys to a file, which is then incorporated in the docs

--- docs/_build/html/_sources/tcutility.data.rst.txt
+++
@@ -1,5 +0,0 @@
-tcutility.data package
-===========================
-
-The ``tcutility.data`` provides useful information necessary for other programs to function. 
-Of course, you can also use this information yourself.
---
+++ docs/utility_doc_scripts/make_result_multikey_files.py
@@ -0,0 +1,30 @@
+﻿import pathlib as pl
+from enum import StrEnum
+
+import tcutility.results as results
+
+
+class OutputFileNames(StrEnum):
+    ADF_CALC = "adf_multikeys.txt"
+
+
+def _format_multikeys(multi_keys: list[str]) -> str:
+    return "\n".join(multi_keys)
+
+
+def main():
+    current_dir = pl.Path(__file__).parent.resolve()
+    output_doc_folder = current_dir.parent / "_static" / "result"
+    path_to_adf_calc_folder = current_dir.parent.parent / "test" / "fixtures" / "ethanol"
+    info = results.read(path_to_adf_calc_folder)
+
+    multi_keys = info.multi_keys()
+
+    for output_file_name in OutputFileNames:
+        formatted_multikeys = _format_multikeys(multi_keys)
+        output_file = output_doc_folder / output_file_name
+        output_file.write_text(formatted_multikeys)
+
+
+if __name__ == "__main__":
+    main()
