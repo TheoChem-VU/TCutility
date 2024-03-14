4e07e6af139b4ae359f338489d51877524f6e694
Yuman Hordijk <yumanhordijk@gmail.com>
2024-03-14 17:54:09 +0100

    Added the tcparser script which will handle generating a parser and running subprograms

---
+++ src/tcutility/cli_scripts/tcparser.py
@@ -0,0 +1,29 @@
+def main():
+	import argparse
+	from tcutility.cli_scripts import job_script, concatenate_irc
+
+	parser = argparse.ArgumentParser(prog='tc')
+	# add the subparsers. dest ensures we can retrieve the subparser name later on
+	subparsers = parser.add_subparsers(dest='subparser_name')
+
+	# to add a script:
+	# 1. Add a create_subparser function and main function to your script.
+	# 2. Import the script.
+	# 3. Add it to the dictionary below {program_name: script-module}.
+	sub_programs = {
+		"optimize": job_script,
+		"concat-irc": concatenate_irc,
+	}
+
+	# add the subparsers to the main parser
+	for sub_program in sub_programs.values():
+		sub_program.create_subparser(subparsers)
+
+	args = parser.parse_args()
+
+	# call the main function of the subprogram that was called
+	sub_programs[args.subparser_name].main(args)
+
+
+if __name__ == '__main__':
+	main()
