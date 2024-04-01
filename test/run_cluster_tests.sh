de0c8150387e797669da8a286a31c4aa1098b82e
tcstud9 <tcstud9@login3.cm.cluster>
2024-04-02 00:15:51 +0200

    Updated the start script to submit the testing script to slurm

--- test/run_cluster_tests.sh
+++ test/run_cluster_tests.sh
@@ -1,7 +1,11 @@
 source ~/.bashrc
 conda activate py
 cd ~/TCutility
-git switch main
+#git switch main
 git pull
-cd test
-pytest cluster_tests >> TCutility_cluster_test_$(date + %F_%T).out
+cd test/cluster_tests
+outf="results/TCutility_cluster_test_$(date +%F_%T).out"
+echo "Git branch $(git branch)" >> outf
+echo "$Git version $(git describe --tags)" >> outf
+
+sbatch --job-name="TCutility $(git describe --tags)" -p tc --wrap="pytest >> $outf"
