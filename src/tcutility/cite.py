ca32192addb6b884ba74770e88252b063b144d07
Yuman Hordijk <yumanhordijk@gmail.com>
2024-04-30 01:07:18 +0200

    Added the tcutility.cite module for generating citations in certain styles

---
+++ src/tcutility/cite.py
@@ -0,0 +1,76 @@
+import requests
+import json
+from tcutility import spell_check, cache
+
+
+
+# @cache.cache_file('dois')
+def _get_doi_data(doi: str) -> dict:
+	'''
+	Get information about an article using the crossref.org API.
+
+	Args:
+		doi: the DOI to get information about.
+	'''
+	data = requests.get(f'http://api.crossref.org/works/{doi}').text
+	data = json.loads(data)
+	return data
+
+
+# @cache.cache_file('journal_abbrvs')
+def _get_journal_abbreviation(journal: str) -> str:
+	'''
+	Get the journal name abbreviation using the abbreviso API.
+
+	Args:
+		journal: the name of the journal to get the abbreviation of.
+	'''
+	return requests.get(f"https://abbreviso.toolforge.org/a/{journal}").text
+
+
+def cite(doi: str, style: str = 'wiley') -> str:
+	'''
+	Format an article in a certain style.
+
+	Args:
+		doi: the article DOI to generate a citation for.
+		style: the style formatting to use. Can be ``['wiley', 'acs', 'rsc']``.
+	'''
+	# check if the style was correctly given
+	spell_check.check(style, ['wiley', 'acs', 'rsc'])
+
+	# get the information about the DOI
+	data = _get_doi_data(doi)
+	
+	# grab usefull data
+	journal = data['message']['container-title'][0]
+	journal_abbreviation = _get_journal_abbreviation(journal)
+	year = data['message']['issued']['date-parts'][0][0]
+	volume = data['message']['volume']
+	pages = data['message'].get('page')
+
+	initials = []
+	last_names = []
+	for author in data['message']['author']:
+		# we get the capital letters from the first names
+		# these will become the initials for this author
+		firsts = [char + '.' for char in author['given'] if char.isupper()]
+		firsts = " ".join(firsts)
+		initials.append(firsts)
+		last_names.append(author['family'])
+
+	# and format the citation correctly
+	if style == 'wiley':
+		names = [f'{first} {last}' for first, last in zip(initials, last_names)]
+		citation = f'{", ".join(names)}, <i>{journal_abbreviation}</i> <b>{year}</b>, <i>{volume}</i>'
+		if pages:
+			citation += f', {pages}'
+		citation += '.'
+
+	elif style == 'acs':
+		raise NotImplementedError()
+
+	elif style == 'rsc':
+		raise NotImplementedError()
+
+	return citation
