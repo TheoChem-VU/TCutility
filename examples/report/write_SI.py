aaa78fcae0d91363b06ec091069d743e9c526284
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-07-14 11:30:03 +0200

    Added relevant example

---
+++ examples/report/write_SI.py
@@ -0,0 +1,10 @@
+import pathlib as pl
+
+from tcutility import formula
+from tcutility.analysis.report import report
+
+pwd = pl.Path(__file__).parent
+with report.SI("test.docx") as si:
+    si.add_heading("Test molecules:")
+    si.add_xyz(str(object=pwd.parent.parent / "test" / "fixtures" / "level_of_theory" / "M06_2X"), title="Cyclo-octatriene")
+    si.add_xyz(str(object=pwd.parent.parent / "test" / "fixtures" / "ethanol"), title=formula.molecule("C2H5OH"))
