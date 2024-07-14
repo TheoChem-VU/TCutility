4564f75a4989f8eccc2e95f26b562d9488c5006b
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2024-07-14 11:29:52 +0200

    Moved files to report and deleted old example that is not supported anymore

--- examples/report/write_xyz_files.py
+++
@@ -1,9 +0,0 @@
-from tcutility.report import SI
-from tcutility._report.formatters import xyz
-
-
-if __name__ == '__main__':
-    with SI('test') as si:
-        si.add_xyz('/Users/yumanhordijk/PhD/Projects/RadicalAdditionBenchmark/data/abinitio_opt/RC_C2H2_NH2/PREOPT', 'RC_C2H2')
-
-    # print(xyz.Default().write('/Users/yumanhordijk/PhD/Projects/RadicalAdditionBenchmark/data/abinitio_opt/RC_C2H2_NH2/PREOPT'))
