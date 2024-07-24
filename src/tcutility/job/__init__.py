8494ebf934617309eb13c77febc6098969e2c753
Yuman Hordijk <yumanhordijk@gmail.com>
2024-07-24 22:58:38 +0200

    Now exposing the `DensfJob` at the `tcutility.job` level

--- src/tcutility/job/__init__.py
+++ src/tcutility/job/__init__.py
@@ -1,7 +1,7 @@
 from . import postscripts  # noqa: F401
 from .generic import Job  # noqa: F401
 from .ams import AMSJob  # noqa: F401
-from .adf import ADFJob, ADFFragmentJob  # noqa: F401
+from .adf import ADFJob, ADFFragmentJob, DensfJob  # noqa: F401
 from .dftb import DFTBJob  # noqa: F401
 from .crest import CRESTJob, QCGJob  # noqa: F401
 from .xtb import XTBJob  # noqa: F401
