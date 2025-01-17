b988761842d721daedc6619cb22aab35a00bd66f
SiebeLeDe <siebbe@hotmail.com>
2025-01-17 19:58:02 +0100

    added resize figure cli script to the tc group

--- src/tcutility/cli_scripts/tcparser.py
+++ src/tcutility/cli_scripts/tcparser.py
@@ -4,6 +4,7 @@ from tcutility.cli_scripts.concatenate_irc import concatenate_irc_paths
 from tcutility.cli_scripts.geo import calculate_geometry_parameter
 from tcutility.cli_scripts.job_script import optimize_geometry
 from tcutility.cli_scripts.read import read_results
+from tcutility.cli_scripts.resize_figures import resize
 
 
 @click.group()
@@ -17,6 +18,7 @@ tc.add_command(optimize_geometry, name="optimize")
 tc.add_command(generate_citations, name="cite")
 tc.add_command(calculate_geometry_parameter, name="geo")
 tc.add_command(concatenate_irc_paths, name="concat-irc")
+tc.add_command(resize, name="resize")
 
 if __name__ == "__main__":
     tc()
