a6f5320a637878c3b289d3e28792055d25d4a8d0
Yuman Hordijk <yumanhordijk@gmail.com>
2025-02-03 10:43:53 +0100

    [#364] Improved the example and added one with unrestricted uvvis

--- examples/job/uvvis.py
+++ examples/job/uvvis.py
@@ -3,12 +3,25 @@ from tcutility.job import ADFJob, ADFFragmentJob
 
 # uv/vis calculations can be run on singlepoint jobs
 with ADFJob() as job:
-	job.molecule('../NH3BH3.xyz')
+	job.rundir = 'uvvis'
+	job.name = 'restricted'
+	job.molecule('NH3BH3.xyz')
 	job.excitations()
 
 # and also fragment jobs
 # in this case the SFOs are simply built up of the fragment densities
 with ADFFragmentJob() as job:
-	job.molecule('../NH3BH3.xyz')
+	job.rundir = 'uvvis'
+	job.name = 'fragment'
+	job.molecule('NH3BH3.xyz')
 	job.excitations()
+
+
+with ADFJob() as job:
+	job.rundir = 'uvvis'
+	job.name = 'unrestricted'
+	job.molecule('NH3BH3.xyz')
+	job.excitations()
+	job.unrestricted(True)
+
 
