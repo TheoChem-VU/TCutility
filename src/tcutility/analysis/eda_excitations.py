0c4fe59a8fc152a44ade6396bb6784754aa63a5b
Yuman Hordijk <yumanhordijk@gmail.com>
2025-02-03 09:55:12 +0100

    [#353] Added a check for restrictedness of calculations

--- src/tcutility/analysis/eda_excitations.py
+++ src/tcutility/analysis/eda_excitations.py
@@ -48,7 +48,7 @@ class EDAExcitation:
 
     def _load_fragment_characters(self) -> dict:
         '''
-        Load the fragment characters of the from-MO and the to-MO for each transition.
+        Determine the fragment characters of the from-MO and the to-MO for each transition.
 
         Returns:
             Dictionary with one key for each fragment. 
@@ -113,10 +113,8 @@ class EDAExcitations:
                     to_MO_spins = exc_data.to_MO_spin[exc_idx]
 
                     if has_pyfmo:
-                        from_orbs = [[mo for mo in self.orbs_complex.mos if mo.name.startswith(MO.strip('_A'))] for MO in from_MO_names]
-                        to_orbs = [[mo for mo in self.orbs_complex.mos if mo.name.startswith(MO.strip('_A'))] for MO in to_MO_names]
-                        # print(self.orbs_complex.mos.orbitals)
-                        # print(from_MO_names)
+                        from_orbs = [[mo for mo in self.orbs_complex.mos if mo.name.startswith(MO)] for MO in from_MO_names]
+                        to_orbs = [[mo for mo in self.orbs_complex.mos if mo.name.startswith(MO)] for MO in to_MO_names]
                     else:
                         from_orbs = None
                         to_orbs = None
@@ -137,95 +135,8 @@ class EDAExcitations:
                                         )
                     self.excitations.append(exc)
 
-                    # # if exc_data.oscillator_strengths[exc_idx] < 1e-6:
-                    # #     continue
 
-                    # exc = EDAExcitation(exc_idx + 1, exc_data.wavelengths[exc_idx], exc_data.energies[exc_idx], exc_data.oscillator_strengths[exc_idx])
-                    # # go through each pair of orbitals
-                    # for i, (forb, torb) in enumerate(zip(from_orbs, to_orbs)):
-                    #     contr = exc_data.contributions[exc_idx][i]
-                    #     if contr < 0.1:
-                    #         continue
-
-                    #     donor_coeffs = np.array([sfo.coefficient(forb) for sfo in donor_sfos])
-                    #     all_coeffs = np.array([sfo.coefficient(forb) for sfo in donor_sfos + acceptor_sfos])
-                    #     donor_character = sum(donor_coeffs**2) / sum(all_coeffs**2)
-                    #     if donor_character < 0.9:
-                    #         continue
-
-                    #     acceptor_coeffs = np.array([sfo.coefficient(torb) for sfo in acceptor_sfos])
-                    #     all_coeffs = np.array([sfo.coefficient(torb) for sfo in donor_sfos + acceptor_sfos])
-                    #     acceptor_character = sum(acceptor_coeffs**2) / sum(all_coeffs**2)
-                    #     if acceptor_character < 0.9:
-                    #         continue
-
-                    #     s = f'{i+1} ({contr: 6.1%}): {forb} ({donor_character: 6.1%}) ⎯► {torb} ({acceptor_character: 6.1%}), '
-                    #     if self.orbs_acc:
-                    #         best_sfo = self.orbs_acc.mos.orbitals[np.argmax(abs(acceptor_coeffs))]
-
-                    #         # check if torb of the sfos are unrestricted
-                    #         if torb.spin in ['A', 'B'] and best_sfo.spin not in ['A', 'B']:
-                    #             best_sfo = self.orbs_acc.mos.orbitals[np.argmax(abs(acceptor_coeffs))//2]
-
-                    #         cbr_character = br_c_score(self.orbs_acc, best_sfo)
-                    #         if abs(cbr_character) < 0.9:
-                    #             continue
-                    #         s += f'{cbr_character: 7.1%} C-Br, '
-                    #     s += f'𝚫ε = {abs(forb.energy - torb.energy): .2f} eV'
-                    #     exc.transitions.append(s)
-
-                    # if len(exc.transitions) > 0:
-                    #     print(exc)
-
-
-def br_c_score(orbs, mo):
-    br_4ps = [orbs.sfos['Br(3P:x)'], orbs.sfos['Br(3P:y)'], orbs.sfos['Br(3P:z)']]
-    c_2ps = [orbs.sfos['C(1P:x)'], orbs.sfos['C(1P:y)'], orbs.sfos['C(1P:z)']]
-    shortest = float('inf')
-    shortest_idx = None
-    for i, ao in enumerate(c_2ps[0]):
-        d = ao.molecule[1].distance_to(br_4ps[0].molecule[1])
-        if d < shortest:
-            shortest = d
-            shortest_idx = i
-
-    c_2ps = [c_2ps[0][shortest_idx], c_2ps[1][shortest_idx], c_2ps[2][shortest_idx]]
-    br_4ps_c = np.array([sfo.coefficient(mo) for sfo in br_4ps])
-    c_2ps_c = np.array([sfo.coefficient(mo) for sfo in c_2ps])
-
-    bv = np.array(br_4ps[0].molecule[1].coords) - np.array(c_2ps[0].molecule[1].coords)
-    bv = bv / np.linalg.norm(bv)
-    br_4ps_c = br_4ps_c / np.linalg.norm(br_4ps_c)
-    c_2ps_c = c_2ps_c / np.linalg.norm(c_2ps_c)
-
-    bv_br = bv @ br_4ps_c
-    bv_c = bv @ c_2ps_c
-
-    return  bv_c * bv_br
-
-
-# if __name__ == '__main__':
-#     import matplotlib.pyplot as plt
-
-#     orbs = pyfmo.orbitals2.objects.Orbitals('frag_uvvis/EDA.Acceptor.results/accetor.adf.rkf')
-#     scores = []
-#     for mo in orbs.mos:
-#         scores.append(br_c_score(orbs, mo))
-#         print(mo, br_c_score(orbs, mo))
-
-#     plt.plot(scores)
-#     plt.show()
 
 if __name__ == '__main__':
     exc = EDAExcitations('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/tmp/EDA')
     print(exc.excitations[0].fragment_characters)
-
-
-# if __name__ == '__main__':
-#     import tkinter as tk
-#     from tkinter import filedialog
-
-#     tk.Tk().withdraw()
-#     kffile = filedialog.askopenfilename(title='Select complex adf.rkf file!')
-#     kffile_acc = filedialog.askopenfilename(title='Select acceptor fragment adf.rkf file!')
-#     EDAExcitations(kffile, kffile_acc)
