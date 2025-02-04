16bd77df2d4f0e80f0d02f81f1428960c41b93c7
Yuman Hordijk <yumanhordijk@gmail.com>
2025-02-04 11:59:10 +0100

    [#372] added the sum-of-angles calculation to the `tc geo` CLI program

--- src/tcutility/cli_scripts/geo.py
+++ src/tcutility/cli_scripts/geo.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Tuple
 
 import click
 from strenum import StrEnum
-from tcutility import geometry, molecule
+from tcutility import geometry, molecule, results
 
 
 class GeometricParameter(StrEnum):
@@ -15,6 +15,7 @@ class GeometricParameter(StrEnum):
     ANGLE = "Angle"
     DIHEDRAL = "Dihedral"
     PYRAMIDAL = "Pyramidal"
+    SUMOFANGLES = 'SumOfAngles'
 
 
 geometric_character_to_info_mapping: Dict[GeometricParameter, Tuple[str, str, int]] = {
@@ -22,32 +23,44 @@ geometric_character_to_info_mapping: Dict[GeometricParameter, Tuple[str, str, in
     GeometricParameter.DISTANCE: ("Distance", " Å", 3),
     GeometricParameter.COORDINATE: ("Angle", "°", 2),
     GeometricParameter.DIHEDRAL: ("Dihedral", "°", 2),
-    GeometricParameter.PYRAMIDAL: ("Pyramidal", "°", 2),
+    GeometricParameter.SUMOFANGLES: ("SumOfAngles", "°", 2),
 }
 
 
 @click.command()
-@click.argument("xyzfile", type=click.Path(exists=True))
+@click.argument("path", type=click.Path(exists=True))
 @click.argument("atom_indices", type=str, nargs=-1)
-@click.option("-p", "--pyramidal", is_flag=True, default=False, help="Instead of calculating dihedral angles, calculate pyramidalisation angle.")
-def calculate_geometry_parameter(xyzfile: str, atom_indices: List[str], pyramidal: bool) -> None:
+@click.option("-p", "--pyramidal", is_flag=True, default=False, help="Instead of calculating a dihedral angle, calculate pyramidalisation angle.")
+@click.option("-s", "--soa", is_flag=True, default=False, help="Instead of calculating a dihedral angle, calculate sum-of-angles angle.")
+def calculate_geometry_parameter(path: str, atom_indices: List[str], pyramidal: bool, soa: bool) -> None:
     """
-    Calculate geometrical parameters for atoms at the provided indices.
-
+    \b
+    Calculate geometrical parameters for atoms at the provided ATOM_INDICES (NB: counting starts at 1).
+    PATH should be an XYZ-file or a calculation directory.
+    
+    \b
     For 1 index this program returns the cartesian coordinate for this atom.
     For 2 indices return bond length between atoms.
     For 3 indices return bond angle, with the second index being the central atom.
-    For 4 indices return dihedral angle by calculating the angle between normal vectors described by atoms at indices 1-2-3 and indices 2-3-4.
-    If the -p/--pyramidal flag is turned on it calculates 360° - ang1 - ang2 - ang3, where ang1, ang2 and ang3 are the angles described by indices 2-1-3, 3-1-4 and 4-1-2 respectively.
+    For 4 indices return dihedral angle by calculating the angle between normal vectors
+        described by atoms at indices 1-2-3 and indices 2-3-4.
+    \b
+    If the -p/--pyramidal flag is turned on it calculates 360° - ang1 - ang2 - ang3, 
+        where ang1, ang2 and ang3 are the angles described by indices 2-1-3, 3-1-4 
+        and 4-1-2 respectively.
+    If the -s/--soa flag is turned on it calculates ang1 + ang2 + ang3.
     """
-    mol = molecule.load(xyzfile)
+    try:
+        mol = molecule.load(path)
+    except IsADirectoryError:
+        mol = results.read(path).molecule.output
     indices = [int(i) - 1 for i in atom_indices]
 
     assert 1 <= len(indices) <= 4, f"Number of indices must be 1, 2, 3 or 4, not {len(indices)}"
 
     atoms = "-".join([f"{mol[i+1].symbol}{i+1}" for i in indices])
 
-    param_value = geometry.parameter(mol, *indices, pyramidal=pyramidal)
+    param_value = geometry.parameter(mol, *indices, pyramidal=pyramidal, sum_of_angles=soa)
 
     if len(indices) == 1:
         param_value = "  ".join([f"{x: .6f}" for x in mol[indices[0] + 1].coords])
@@ -56,10 +69,13 @@ def calculate_geometry_parameter(xyzfile: str, atom_indices: List[str], pyramida
         param_type = GeometricParameter.DISTANCE
     elif len(indices) == 3:
         param_type = GeometricParameter.ANGLE
-    elif len(indices) == 4 and not pyramidal:
-        param_type = GeometricParameter.DIHEDRAL
-    elif len(indices) == 4 and pyramidal:
-        param_type = GeometricParameter.PYRAMIDAL
+    elif len(indices) == 4:
+        if pyramidal:
+            param_type = GeometricParameter.PYRAMIDAL
+        elif soa:
+            param_type = GeometricParameter.SUMOFANGLES
+        else:
+            param_type = GeometricParameter.DIHEDRAL
     else:
         raise ValueError("Invalid number of indices")
 
