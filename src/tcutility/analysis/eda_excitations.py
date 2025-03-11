69a717125fbb32c2e2668e4353459a8a1c3f50fc
Yuman Hordijk <yumanhordijk@gmail.com>
2025-03-11 11:49:44 +0100

    Added gathering of transitions for each excitation. Also reading excitation type and irrep

--- src/tcutility/analysis/eda_excitations.py
+++ src/tcutility/analysis/eda_excitations.py
@@ -9,14 +9,48 @@ except ModuleNotFoundError:
     has_pyfmo = False
 
 
-# class EDATransition:
-#     def __init__(self,
-#                  ):
+class EDATransition:
+    def __init__(self,
+                 from_MO_name=None,
+                 to_MO_name=None,
+                 contribution=None,
+                 from_MO=None,
+                 to_MO=None,
+                 ):
+        self.from_MO_name = from_MO_name
+        self.to_MO_name = to_MO_name
+        self.contribution = contribution
+        self.from_MO = from_MO
+        self.to_MO = to_MO
+
+    def __str__(self):
+        fname = self.from_MO_name
+        if '_AB' in fname:
+            fname = fname.removesuffix('_AB')
+        else:
+            fname = fname.replace('_A', ' α').replace('_B', ' ß')
+
+        tname = self.to_MO_name
+        if '_AB' in tname:
+            tname = tname.removesuffix('_AB')
+        else:
+            tname = tname.replace('_A', ' α').replace('_B', ' ß')
+
+        if self.from_MO is not None:
+            fname = f'{fname} ({self.from_MO[0].energy:.1f} eV)'
+
+        if self.to_MO is not None:
+            tname = f'{tname} ({self.to_MO[0].energy:.1f} eV)'
+
+        s = f'{self.contribution:6.1%} {fname} -> {tname}'
+        return s
 
 
 class EDAExcitation:
     def __init__(self, 
                  exc_idx=-1, 
+                 exc_type=None, 
+                 exc_irrep=None, 
                  wavelength=0, 
                  photon_energy=0, 
                  osc_strength=0,
@@ -26,10 +60,14 @@ class EDAExcitation:
                  to_MO_spins=None,
                  from_orbs=None,
                  to_orbs=None,
+                 contributions=None,
                  fragments=None,
                  orbs_complex=None,
-                 orbs_frags=None):
+                 orbs_frags=None
+                 ):
         self.exc_idx = exc_idx
+        self.exc_type = exc_type
+        self.exc_irrep = exc_irrep
         self.wavelength = wavelength
         self.photon_energy = photon_energy * 27.2107  # Ha to eV
         self.osc_strength = osc_strength
@@ -39,14 +77,16 @@ class EDAExcitation:
         self.to_MO_spins = to_MO_spins
         self.from_orbs = from_orbs
         self.to_orbs = to_orbs
+        self.contributions = contributions
         self.fragments = fragments
         self.orbs_frags = orbs_frags
         self.orbs_complex = orbs_complex
         if has_pyfmo:
             self._load_fragment_characters()
         self.transitions = []
+        self._load_transitions()
 
-    def _load_fragment_characters(self) -> dict:
+    def _load_fragment_characters(self):
         '''
         Determine the fragment characters of the from-MO and the to-MO for each transition.
 
@@ -74,8 +114,30 @@ class EDAExcitation:
 
                 self.fragment_characters[frag].append((forb_character, torb_character))
 
+    def _load_transitions(self):
+        ntransitions = len(self.from_MO_names)
+        for i in range(ntransitions):
+            # check if there are transitions with the same names
+            # if there is we should add the contributions for those transitions
+            # instead of adding a new one
+            from_MO_name = self.from_MO_names[i]
+            to_MO_name = self.to_MO_names[i]
+            for trans in self.transitions:
+                if trans.from_MO_name == from_MO_name and trans.to_MO_name == to_MO_name:
+                    trans.contribution += self.contributions[i]
+                    break
+            else:
+                T = EDATransition(
+                    from_MO_name=from_MO_name, 
+                    to_MO_name=to_MO_name,
+                    contribution=self.contributions[i])
+                if has_pyfmo:
+                    T.from_MO = self.from_orbs[i]
+                    T.to_MO = self.to_orbs[i]
+                self.transitions.append(T)
+
     def __str__(self):
-        s = f'''Excitation({self.exc_idx}):
+        s = f'''Excitation({self.exc_idx}[{self.exc_irrep}], {self.exc_type}):
     λ   = {self.wavelength: .2f} nm
     h𝛎  = {self.photon_energy: .3f} eV
     f12 = {self.osc_strength: .4f} km/mol
@@ -95,6 +157,9 @@ class EDAExcitations:
             self.res_frags = {frag: results.read(os.path.join(calc_dir, f'frag_{frag}')) for frag in frags}
             self.orbs_frags = {frag: pyfmo.orbitals2.objects.Orbitals(self.res_frags[frag].files['adf.rkf']) for frag in frags}
         self._load()
+
+    def __iter__(self):
+        return iter(self.excitations)
 
     def _load(self):
         self.excitations = []
@@ -118,24 +183,31 @@ class EDAExcitations:
                         from_orbs = None
                         to_orbs = None
 
-                    exc = EDAExcitation(exc_idx=exc_idx + 1, 
-                                        wavelength=wavelength, 
-                                        photon_energy=energy, 
-                                        osc_strength=osc_strength,
-                                        from_MO_names=from_MO_names,
-                                        to_MO_names=to_MO_names,
-                                        from_MO_spins=from_MO_spins,
-                                        to_MO_spins=to_MO_spins,
-                                        from_orbs=from_orbs,
-                                        to_orbs=to_orbs,
-                                        fragments=self.orbs_complex.fragments,
-                                        orbs_complex=self.orbs_complex,
-                                        orbs_frags=self.orbs_frags,
-                                        )
+                    kwargs = dict(
+                        exc_idx=exc_idx + 1,
+                        exc_type=exc_type,
+                        exc_irrep=exc_irrep,
+                        wavelength=wavelength, 
+                        photon_energy=energy, 
+                        osc_strength=osc_strength,
+                        from_MO_names=from_MO_names,
+                        to_MO_names=to_MO_names,
+                        from_MO_spins=from_MO_spins,
+                        to_MO_spins=to_MO_spins,
+                        from_orbs=from_orbs,
+                        to_orbs=to_orbs,
+                        contributions=contributions)
+                    if has_pyfmo:
+                        kwargs['fragments'] = self.orbs_complex.fragments
+                        kwargs['orbs_complex'] = self.orbs_complex
+                        kwargs['orbs_frags'] = self.orbs_frags
+
+                    exc = EDAExcitation(**kwargs)
                     self.excitations.append(exc)
 
 
-
 if __name__ == '__main__':
-    exc = EDAExcitations('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/fragment_unrestricted')
-    print(exc.excitations[0].fragment_characters)
+    excs = EDAExcitations('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/fragment_unrestricted')
+    # print(exc.excitations[0].fragment_characters)
+    for exc in excs:
+        print(exc)
