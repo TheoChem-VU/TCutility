8e570e7345b4f6e867548cf04d9d4db2ffda1758
Yuman Hordijk <yumanhordijk@gmail.com>
2025-04-22 11:42:57 +0200

    Added writing mode to the `open_file` methods of connections

--- src/tcutility/connect.py
+++ src/tcutility/connect.py
@@ -245,11 +245,11 @@ class Connection:
 
 
 class ServerFile:
-    def __init__(self, file_path: str, server: Connection):
+    def __init__(self, file_path: str, server: Connection, mode: str = 'w+'):
         self.server = server
         self.file_path = file_path
         self.tmp_path = str(uuid.uuid4())
-        self.file = open(self.tmp_path, mode='w+')
+        self.file = open(self.tmp_path, mode=mode)
 
     def __enter__(self):
         return self.file
@@ -322,8 +322,9 @@ class Local:
     def path_exists(self, path: str) -> bool:
         return os.path.exists(os.path.join(self.currdir, path))
 
-    def open_file(self, file_path: str):
-        return open(file_path, mode='w+')
+    def open_file(self, file_path: str, mode:str = 'w+'):
+        print(file_path, mode)
+        return open(file_path, mode=mode)
 
     def chmod(self, rights: int, path: str):
         os.chmod(os.path.join(self.currdir, path), int(str(rights), base=8))
