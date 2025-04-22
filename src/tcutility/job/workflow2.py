cf17e2743a790720d78216a8aa994ecb956e5695
Yuman Hordijk <yumanhordijk@gmail.com>
2025-04-22 16:02:30 +0200

    Added missing quotation marks to squeue call

--- src/tcutility/job/workflow2.py
+++ src/tcutility/job/workflow2.py
@@ -247,12 +247,13 @@ def ruff_check_script(path: str, ignored_codes=None) -> bool:
 
 
 @workflow(
-    sbatch={'p': 'rom', 'n': 32, 't': '120:00:00'},
-    delete_files=True,
+    sbatch={'p': 'rome', 'n': 32, 't': '120:00:00'},
+    delete_files=False,
     )
 def sn2(molecule: 'path' = (1, 2, 3)) -> None:
     import tcutility
-
+    print('Test')
+    
     with tcutility.job.DFTBJob(use_slurm=False) as job:
         job.molecule(molecule)
 
