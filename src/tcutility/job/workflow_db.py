575603d2ac94ba716a2d467fc56cd6af2001e658
Luca <ld.verduin@gmail.com>
2025-04-29 12:52:25 +0200

    Now stripping values for '\n' in workflow.db.get_info()

--- src/tcutility/job/workflow_db.py
+++ src/tcutility/job/workflow_db.py
@@ -32,7 +32,7 @@ def get_data(hsh):
 		data['status'] = status.strip()
 		for part in parts[2:]:
 			k, v = part.split('=')
-			data[k.strip()] = v
+			data[k.strip()] = v.strip('\n')
 
 	return data
 
