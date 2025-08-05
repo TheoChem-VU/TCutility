f8b4f148948cd226d0c0c5a8834840a8e4381e2d
Yuman Hordijk <yumanhordijk@gmail.com>
2025-08-05 02:13:38 +0200

    fixing ruff issues

--- src/tcutility/results2/__init__.py
+++ src/tcutility/results2/__init__.py
@@ -2,6 +2,6 @@ from . import *  # noqa: F403
 
 
 def read(path: str) -> result.NestedDict:
-	program = detect_filetype.detect_program(path)
+	program = detect_filetype.detect_program(path)  # noqa: F405
 	if program == 'adf':
-		return adf.read_adfrkf(path)
+		return adf.read_adfrkf(path)  # noqa: F405
