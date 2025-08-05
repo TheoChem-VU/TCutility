f60bd05ba16a11cd2b72ceb84252673af6df8571
Yuman Hordijk <yumanhordijk@gmail.com>
2025-08-05 02:12:15 +0200

    fixing ruff issues

--- src/tcutility/results2/adf.py
+++ src/tcutility/results2/adf.py
@@ -1,5 +1,4 @@
 from tcutility.results2 import result, adf_rkf
-from tcutility import cache, results, ensure_list
 import numpy as np
 
 
@@ -14,44 +13,44 @@ def read_adfrkf(path: str, ret: result.NestedDict = None):
     return ret
 
 
-if __name__ == '__main__':
-    def equal_dicts(d1: dict, d2: dict) -> bool:
-        '''
-        Test if two dictionaries are the same
-        '''
-        import dictfunc
-        for row1, row2 in zip(dictfunc.dict_to_list(d1), dictfunc.dict_to_list(d2)):
-            for x, y in zip(row1, row2):
-                if isinstance(x, np.ndarray):
-                    if (x != y).any():
-                        return False
-                else:
-                    if x != y:
-                        return False
-        return True
+# if __name__ == '__main__':
+#     def equal_dicts(d1: dict, d2: dict) -> bool:
+#         '''
+#         Test if two dictionaries are the same
+#         '''
+#         import dictfunc
+#         for row1, row2 in zip(dictfunc.dict_to_list(d1), dictfunc.dict_to_list(d2)):
+#             for x, y in zip(row1, row2):
+#                 if isinstance(x, np.ndarray):
+#                     if (x != y).any():
+#                         return False
+#                 else:
+#                     if x != y:
+#                         return False
+#         return True
 
-    ref_res = results.read('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results')
-    test_res = read_adfrkf('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results/adf.rkf')
+#     ref_res = results.read('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results')
+#     test_res = read_adfrkf('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results/adf.rkf')
 
-    # print(equal_dicts(ref_res.adf, test_res.adf))
-    # print(equal_dicts(ref_res.properties.excitations, test_res.get('properties', 'excitations')))
+#     # print(equal_dicts(ref_res.adf, test_res.adf))
+#     # print(equal_dicts(ref_res.properties.excitations, test_res.get('properties', 'excitations')))
 
-    import pprint
+#     import pprint
 
-    # pprint.pprint(ref_res.adf)
-    pprint.pprint(test_res['properties']['excitations'])
+#     # pprint.pprint(ref_res.adf)
+#     pprint.pprint(test_res['properties']['excitations'])
 
 
-    ref_res = results.read('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results')
-    test_res = read_adfrkf('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results/adf.rkf')
-    # print(test_res)
+#     ref_res = results.read('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results')
+#     test_res = read_adfrkf('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results/adf.rkf')
+#     # print(test_res)
 
-    # print(equal_dicts(ref_res.adf, test_res.get('adf')))
+#     # print(equal_dicts(ref_res.adf, test_res.get('adf')))
 
-    import pprint
+#     import pprint
 
-    # pprint.pprint(ref_res.adf)
-    pprint.pprint(test_res['properties']['vibrations'])
+#     # pprint.pprint(ref_res.adf)
+#     pprint.pprint(test_res['properties']['vibrations'])
 
-    # print(equal_dicts(ref_res.properties.vibrations, test_res.get('properties', 'vibrations')))
+#     # print(equal_dicts(ref_res.properties.vibrations, test_res.get('properties', 'vibrations')))
 
