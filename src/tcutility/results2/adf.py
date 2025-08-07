b6412f7e1edec69e695599df5a5905a5a778d4a6
Yuman Hordijk <yumanhordijk@gmail.com>
2025-08-07 13:12:55 +0200

    Added reading of the output file

--- src/tcutility/results2/adf.py
+++ src/tcutility/results2/adf.py
@@ -1,10 +1,15 @@
-from tcutility.results2 import result, adf_rkf
+from tcutility.results2 import result, adf_rkf, adf_out
 
 
 def read_adfrkf(path: str, ret: result.NestedDict = None):
     if ret is None:
         ret = result.NestedDict()
 
+    ret.set('engine', 'adf')
+    ret.set('status', adf_rkf._read_status(path))
+    ret.set('job_id', adf_rkf._read_jobid(path))
+    ret.set('input', 'adf', adf_rkf._read_adf_input(path))
+    ret.set('level', adf_rkf._read_level_of_theory(path))
     ret.set('adf', adf_rkf._read_settings(path))
     ret.set('properties', 'excitations', adf_rkf._read_excitations(path))
     ret.set('properties', 'vibrations', adf_rkf._read_vibrations(path))
@@ -12,44 +17,45 @@ def read_adfrkf(path: str, ret: result.NestedDict = None):
     return ret
 
 
-# if __name__ == '__main__':
-#     def equal_dicts(d1: dict, d2: dict) -> bool:
-#         '''
-#         Test if two dictionaries are the same
-#         '''
-#         import dictfunc
-#         for row1, row2 in zip(dictfunc.dict_to_list(d1), dictfunc.dict_to_list(d2)):
-#             for x, y in zip(row1, row2):
-#                 if isinstance(x, np.ndarray):
-#                     if (x != y).any():
-#                         return False
-#                 else:
-#                     if x != y:
-#                         return False
-#         return True
+def read_adfout(path: str, ret: result.NestedDict = None):
+    if ret is None:
+        ret = result.NestedDict()
 
-#     ref_res = results.read('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results')
-#     test_res = read_adfrkf('/Users/yumanhordijk/PhD/Projects/Students/felix_research/test/Excitations_Approx/M06-2X_range/exc.results/adf.rkf')
-
-#     # print(equal_dicts(ref_res.adf, test_res.adf))
-#     # print(equal_dicts(ref_res.properties.excitations, test_res.get('properties', 'excitations')))
-
-#     import pprint
-
-#     # pprint.pprint(ref_res.adf)
-#     pprint.pprint(test_res['properties']['excitations'])
+    ret.set('engine', 'adf')
+    ret.set('job_id', adf_out._read_jobid(path))
+    # ret.set('adf', adf_out._read_settings(path))
+    ret.set('input', adf_out._read_input(path))
+    ret.set('properties', adf_out._read_properties(path))
+    return ret
 
 
-#     ref_res = results.read('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results')
-#     test_res = read_adfrkf('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/test/fixtures/ethanol/geometry_optimization.results/adf.rkf')
-#     # print(test_res)
+if __name__ == '__main__':
+    # from pprint import pprint
+    # from tcutility import results
+    from time import perf_counter
 
-#     # print(equal_dicts(ref_res.adf, test_res.get('adf')))
+    # start = perf_counter()
+    # ret = read_adfrkf('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/restricted/adf.rkf')
+    # print(perf_counter() - start)
 
-#     import pprint
+    start = perf_counter()
+    ret = read_adfout('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/restricted/restricted.out')
+    print(perf_counter() - start)
 
-#     # pprint.pprint(ref_res.adf)
-#     pprint.pprint(test_res['properties']['vibrations'])
+    # start = perf_counter()
+    # ref_ret = results.read('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/restricted')
+    # print(perf_counter() - start)
 
-#     # print(equal_dicts(ref_res.properties.vibrations, test_res.get('properties', 'vibrations')))
+    # start = perf_counter()
+    # ref_ret = results.quick_status('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/restricted')
+    # print(perf_counter() - start)
 
+
+
+
+
+    from time import perf_counter
+    start = perf_counter()
+    import rust_outp
+    rust_outp.read_adf_output('/Users/yumanhordijk/PhD/Programs/TheoCheM/TCutility/examples/job/uvvis/restricted/restricted.out')
+    print(perf_counter() - start)
