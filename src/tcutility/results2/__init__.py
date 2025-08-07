a69f215775762cc1afc290f149501fc0beb32f4d
Yuman Hordijk <yumanhordijk@gmail.com>
2025-08-07 13:12:34 +0200

    Imported specific parts of a the results2 module

--- src/tcutility/results2/__init__.py
+++ src/tcutility/results2/__init__.py
@@ -1,5 +1,5 @@
 from . import *  # noqa: F403
-
+from . import result, detect_filetype
 
 def read(path: str) -> result.NestedDict:  # noqa: F405
 	program = detect_filetype.detect_program(path)  # noqa: F405
