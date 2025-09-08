7c1fff04e58261a12ee68dccf07dc3ae50745ef0
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2025-09-08 14:59:07 +0200

    Refactor test setup to use specific imports from tcutility.job and improve readability

--- test/job/test_setup.py
+++ test/job/test_setup.py
@@ -1,20 +1,24 @@
-from tcutility import job as tcjob
 import os
+
+from tcutility.job import adf
+
 j = os.path.join
 s = os.path.split
 
 
 def test_ADFJob():
-	with tcjob.ADFJob(delete_on_finish=True, test_mode=True) as job:
-		job.name = '.ADFJob'
+    with adf.ADFJob(delete_on_finish=True, test_mode=True) as job:
+        job.name = ".ADFJob"
+
+
+# TODO The job makes a tmp folder which is not deleted for some reason
+
 
 def test_ADFFragmentJob():
-	with tcjob.ADFFragmentJob(delete_on_finish=True, test_mode=True) as job:
-		job.name = '.ADFFragmentJob'
-		job.molecule(j(s(s(__file__)[0])[0], "fixtures", "xyz", "NaCl_homolytic.xyz"))
+    with adf.ADFFragmentJob(delete_on_finish=True, test_mode=True) as job:
+        job.name = ".ADFFragmentJob"
+        job.molecule(j(s(s(__file__)[0])[0], "fixtures", "xyz", "NaCl_homolytic.xyz"))
 
 
-if __name__ == '__main__':
-	import pytest
-
-	pytest.main()
+if __name__ == "__main__":
+    test_ADFFragmentJob()
