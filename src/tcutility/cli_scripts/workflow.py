cc4fa52cee321d7bfc6ef8ddfd2ff0786bb628d4
Yuman Hordijk <yumanhordijk@gmail.com>
2026-02-26 21:28:05 +0100

    [#517] The workflow CLI program can now print the status of ALL workflows

--- src/tcutility/cli_scripts/workflow.py
+++ src/tcutility/cli_scripts/workflow.py
@@ -18,27 +18,53 @@ def workflow():
 def status(use_hash: bool = False, name: str = None):
     if use_hash:
         print(workflow_db.get_status(name))
-    else:
-        log.print_date = False
-        rows = []
-        status_counts = {}
-        for hsh, data in workflow_db.read_all().items():
-            if data.get('workflow_name', None) == name:
-                status = data.get('status', 'UNKOWN')
-                status_counts.setdefault(status, 0)
-                status_counts[status] += 1
+        return
+
+    log.print_date = False
+    rows = []
+    status_counts = {}
+    workflow_name_counts = {}
+    for hsh, data in workflow_db.read_all().items():
+        if name is None or data.get('workflow_name', None) == name:
+            status = data.get('status', 'UNKOWN')
+            status_counts.setdefault(status, 0)
+            status_counts[status] += 1
+
+            workflow_name = data.get('workflow_name', '')
+            workflow_name_counts.setdefault(workflow_name, 0)
+            workflow_name_counts[workflow_name] += 1
+
+            if name is None:
+                rows.append((status, workflow_name, hsh))
+            else:
                 rows.append((status, hsh))
 
-        if len(rows) == 0:
+    if len(rows) == 0:
+        if name is None:
+            print(f'I could not find any workflow runs.')
+        else:
             print(f'I could not find any runs for WorkFlow({name}).')
-            return
-            
+        return
 
-        print(f'Found {sum(list(status_counts.values()))} total run(s) for WorkFlow({name}):')
-        for status, nums in status_counts.items():
-            print(f'  {nums:>5} {status}')
-
+    if name is None:
+        print(f'Found {sum(list(status_counts.values()))} total run(s).')
         print()
+        print(f' From the following workflows:')
+        for workflow_name, nums in workflow_name_counts.items():
+            print(f'  {nums:>5} {workflow_name}')
+
+    else:
+        print(f'Found {sum(list(status_counts.values()))} total run(s) for WorkFlow({name}):')
+
+    print()
+    print(f' With the following statuses:')
+    for status, nums in status_counts.items():
+        print(f'  {nums:>5} {status}')
+
+    print()
+    if name is None:
+        log.table(rows, header=('Status', 'Workflow', 'Hash'))
+    else:
         log.table(rows, header=('Status', 'Hash'))
 
 workflow.add_command(status)
